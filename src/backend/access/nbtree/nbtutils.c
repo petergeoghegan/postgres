80754041b4bf676d61e3b8a1475e7ae9d2497ebd
Peter Geoghegan <pg@bowt.ie>
2019-03-25 12:01:26 -0700

    Force v3 indexes for testing

--- src/backend/access/nbtree/nbtutils.c
+++ src/backend/access/nbtree/nbtutils.c
@@ -154,8 +154,11 @@ _bt_mkscankey(Relation rel, IndexTuple itup)
 		_bt_metaversion(rel, &key->heapkeyspace, &key->allequalimage);
 	else
 	{
-		/* Utility statement callers can set these fields themselves */
-		key->heapkeyspace = true;
+		/*
+		 * XXX Continue to work with v4 indexes, while making sure new indexes are
+		 * v3 indexes
+		 */
+		key->heapkeyspace = false;
 		key->allequalimage = false;
 	}
 	key->anynullkeys = false;	/* initial assumption */
@@ -5160,6 +5163,9 @@ bool
 _bt_allequalimage(Relation rel, bool debugmessage)
 {
 	bool		allequalimage = true;
+
+	/* v3 indexes don't support allequalimage */
+	return false;
 
 	/* INCLUDE indexes can never support deduplication */
 	if (IndexRelationGetNumberOfAttributes(rel) !=
