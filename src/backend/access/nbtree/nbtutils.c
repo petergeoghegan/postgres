01b24e19108e4c194c011f1590cf5a19c103646d
Peter Geoghegan <pg@bowt.ie>
2019-06-17 15:42:19 -0700

    Index scan, page split, and index tuple deletion LOG instrumentation
    Remember: log_btree_verbosity GUC can be set to 1 or 2 to see ultra-fine
    details, such as each btgettuple() call.  Useful whenever you want to
    link the nbtree details that you see to the index AM API/executor/query
    view of things.
    
    Also instrument page splits.  Includes information about choices that
    nbtsplitloc.c could have made but didn't.

--- src/backend/access/nbtree/nbtutils.c
+++ src/backend/access/nbtree/nbtutils.c
@@ -222,8 +222,19 @@ _bt_preprocess_array_keys(IndexScanDesc scan)
 	for (i = 0; i < numberOfKeys; i++)
 	{
 		cur = &scan->keyData[i];
+
 		if (cur->sk_flags & SK_SEARCHARRAY)
 		{
+			if (so->log_btree_verbosity)
+			{
+				char *flags = dump_scankey_flags(cur);
+				appendStringInfo(&so->debugstr,
+								 "_bt_preprocess_array_keys: SK_SEARCHARRAY input %d: [ flags: %s, attno: %u func OID: %u ]\n",
+								 i, flags, cur->sk_attno,
+								 cur->sk_func.fn_oid);
+				pfree(flags);
+			}
+
 			numArrayKeys++;
 			Assert(!(cur->sk_flags & (SK_ROW_HEADER | SK_SEARCHNULL | SK_SEARCHNOTNULL)));
 			/* If any arrays are null as a whole, we can quit right now. */
@@ -239,6 +250,9 @@ _bt_preprocess_array_keys(IndexScanDesc scan)
 	/* Quit if nothing to do. */
 	if (numArrayKeys == 0)
 	{
+		if (so->log_btree_verbosity)
+			appendStringInfo(&so->debugstr,
+							 "_bt_preprocess_array_keys: numArrayKeys == 0 so nothing to do\n");
 		so->numArrayKeys = 0;
 		so->arrayKeyData = NULL;
 		return;
@@ -363,10 +377,27 @@ _bt_preprocess_array_keys(IndexScanDesc scan)
 		so->arrayKeys[numArrayKeys].scan_key = i;
 		so->arrayKeys[numArrayKeys].num_elems = num_elems;
 		so->arrayKeys[numArrayKeys].elem_values = elem_values;
+
+		/*
+		 * Note: Will report elem_values (array element values for scankey) in
+		 * _bt_start_array_keys and _bt_advance_array_keys, rather than doing
+		 * that here
+		 */
+		if (so->log_btree_verbosity)
+			appendStringInfo(&so->debugstr,
+							 "_bt_preprocess_array_keys: scan_key: %d, num_elems: %d\n",
+							 so->arrayKeys[numArrayKeys].scan_key,
+							 so->arrayKeys[numArrayKeys].num_elems);
+
 		numArrayKeys++;
 	}
 
 	so->numArrayKeys = numArrayKeys;
+
+	if (so->log_btree_verbosity)
+		appendStringInfo(&so->debugstr,
+						 "                           so->numArrayKeys is %d, so->arrayKeyCount is %d\n",
+						 so->numArrayKeys, so->arrayKeyCount);
 
 	MemoryContextSwitchTo(oldContext);
 }
@@ -531,6 +562,9 @@ _bt_start_array_keys(IndexScanDesc scan, ScanDirection dir)
 	{
 		BTArrayKeyInfo *curArrayKey = &so->arrayKeys[i];
 		ScanKey		skey = &so->arrayKeyData[curArrayKey->scan_key];
+		Oid			typOutput;
+		bool		varlenatype;
+		char	   *val;
 
 		Assert(curArrayKey->num_elems > 0);
 		if (ScanDirectionIsBackward(dir))
@@ -538,6 +572,25 @@ _bt_start_array_keys(IndexScanDesc scan, ScanDirection dir)
 		else
 			curArrayKey->cur_elem = 0;
 		skey->sk_argument = curArrayKey->elem_values[curArrayKey->cur_elem];
+
+		if (so->log_btree_verbosity)
+		{
+			if (skey->sk_subtype != InvalidOid)
+				getTypeOutputInfo(skey->sk_subtype,
+								  &typOutput, &varlenatype);
+			else
+				getTypeOutputInfo(scan->indexRelation->rd_opcintype[i],
+								  &typOutput, &varlenatype);
+			val = OidOutputFunctionCall(typOutput, skey->sk_argument);
+			appendStringInfo(&so->debugstr,
+							 "_bt_start_array_keys: cur_elem %u, sk_attno: %d, val: %s [%s, %s]\n",
+							 curArrayKey->cur_elem,
+							 skey->sk_attno, val,
+							 (skey->sk_flags & SK_BT_NULLS_FIRST) != 0 ? "NULLS FIRST" : "NULLS LAST",
+							 (skey->sk_flags & SK_BT_DESC) != 0 ? "DESC" : "ASC");
+			if (val)
+				pfree(val);
+		}
 	}
 }
 
@@ -594,12 +647,41 @@ _bt_advance_array_keys(IndexScanDesc scan, ScanDirection dir)
 		curArrayKey->cur_elem = cur_elem;
 		skey->sk_argument = curArrayKey->elem_values[cur_elem];
 		if (found)
+		{
+			Oid			typOutput;
+			bool		varlenatype;
+			char	   *val;
+
+			if (so->log_btree_verbosity)
+			{
+				if (skey->sk_subtype != InvalidOid)
+					getTypeOutputInfo(skey->sk_subtype,
+									  &typOutput, &varlenatype);
+				else
+					getTypeOutputInfo(scan->indexRelation->rd_opcintype[i],
+									  &typOutput, &varlenatype);
+				val = OidOutputFunctionCall(typOutput, skey->sk_argument);
+				appendStringInfo(&so->debugstr,
+								 "_bt_advance_array_keys: cur_elem %u, sk_attno: %d, val: %s [%s, %s]\n",
+								 curArrayKey->cur_elem,
+								 skey->sk_attno, val,
+								 (skey->sk_flags & SK_BT_NULLS_FIRST) != 0 ? "NULLS FIRST" : "NULLS LAST",
+								 (skey->sk_flags & SK_BT_DESC) != 0 ? "DESC" : "ASC");
+				if (val)
+					pfree(val);
+			}
+
 			break;
+		}
 	}
 
 	/* advance parallel scan */
 	if (scan->parallel_scan != NULL)
 		_bt_parallel_advance_array_keys(scan);
+
+	if (!found && so->log_btree_verbosity)
+		appendStringInfo(&so->debugstr,
+						 "_bt_advance_array_keys: failed to find new cur_elem\n");
 
 	return found;
 }
@@ -772,7 +854,13 @@ _bt_preprocess_keys(IndexScanDesc scan)
 	so->numberOfKeys = 0;
 
 	if (numberOfKeys < 1)
+	{
+		if (so->log_btree_verbosity)
+			appendStringInfo(&so->debugstr,
+							 "_bt_preprocess_keys: qual-less scan (i.e. numberOfKeys is 0)\n");
+
 		return;					/* done if qual-less scan */
+	}
 
 	/*
 	 * Read so->arrayKeyData if array keys are present, else scan->keyData
@@ -788,6 +876,22 @@ _bt_preprocess_keys(IndexScanDesc scan)
 	if (cur->sk_attno < 1)
 		elog(ERROR, "btree index keys must be ordered by attribute");
 
+	if (so->log_btree_verbosity)
+	{
+		for (int inkeysn = 0; inkeysn < scan->numberOfKeys; inkeysn++)
+		{
+			ScanKey j = inkeys + inkeysn;
+			char *flags = dump_scankey_flags(j);
+
+			appendStringInfo(&so->debugstr,
+							 "_bt_preprocess_keys: input inkeys[%d]: [ flags: %s, attno: %u func OID: %u ]\n",
+							 inkeysn, flags, cur->sk_attno,
+							 cur->sk_func.fn_oid);
+
+			pfree(flags);
+		}
+	}
+
 	/* We can short-circuit most of the work if there's just one key */
 	if (numberOfKeys == 1)
 	{
@@ -799,6 +903,12 @@ _bt_preprocess_keys(IndexScanDesc scan)
 		/* We can mark the qual as required if it's for first index col */
 		if (cur->sk_attno == 1)
 			_bt_mark_scankey_required(outkeys);
+
+		if (so->log_btree_verbosity)
+			appendStringInfo(&so->debugstr,
+							 "_bt_preprocess_keys: numberOfKeys == 1 case set so->keyData[0] to %lu, qual_ok to %d\n",
+							 outkeys[0].sk_argument, so->qual_ok);
+
 		return;
 	}
 
@@ -1013,6 +1123,11 @@ _bt_preprocess_keys(IndexScanDesc scan)
 			}
 		}
 	}
+
+	if (so->log_btree_verbosity)
+		appendStringInfo(&so->debugstr,
+						 "_bt_preprocess_keys: so->numberOfKeys will be %d\n",
+						 so->numberOfKeys);
 
 	so->numberOfKeys = new_numberOfKeys;
 }
@@ -1738,6 +1853,7 @@ _bt_killitems(IndexScanDesc scan)
 	int			numKilled = so->numKilled;
 	bool		killedsomething = false;
 	bool		droppedpin PG_USED_FOR_ASSERTS_ONLY;
+	int			nkilled = 0;
 
 	Assert(BTScanPosIsValid(so->currPos));
 
@@ -1879,6 +1995,7 @@ _bt_killitems(IndexScanDesc scan)
 				/* found the item/all posting list items */
 				ItemIdMarkDead(iid);
 				killedsomething = true;
+				nkilled++;
 				break;			/* out of inner search loop */
 			}
 			offnum = OffsetNumberNext(offnum);
@@ -1896,6 +2013,11 @@ _bt_killitems(IndexScanDesc scan)
 	{
 		opaque->btpo_flags |= BTP_HAS_GARBAGE;
 		MarkBufferDirtyHint(so->currPos.buf, true);
+
+		if (so->log_btree_verbosity)
+			appendStringInfo(&so->debugstr,
+							 "_bt_killitems: killed %d out of %d numKilled from kill_prior_tuple heapam state\n",
+							 nkilled, numKilled);
 	}
 
 	_bt_unlockbuf(scan->indexRelation, so->currPos.buf);
@@ -2457,6 +2579,60 @@ _bt_keep_natts_fast(Relation rel, IndexTuple lastleft, IndexTuple firstright)
 
 	return keepnatts;
 }
+
+char *
+_nbtree_print_itup(IndexTuple itup,
+				   Relation rel)
+{
+	bool		isnull[INDEX_MAX_KEYS];
+	Datum		values[INDEX_MAX_KEYS];
+	char	   *lkey_desc = NULL;
+	TupleDesc	itupdesc = RelationGetDescr(rel);
+	int			natts;
+	int			orig_natts = itupdesc->natts;
+	int			orig_indnkeyatts = rel->rd_index->indnkeyatts;
+
+	if (!rel->rd_isvalid || RelationGetSmgr(rel) == NULL)
+		return NULL;
+
+	if (rel->rd_isnailed)
+	{
+		/*
+		 * pg_class_oid_index (and likely other indexes) have issues with
+		 * buffer lock self-deadlocks, so punt in a hacky way.
+		 *
+		 * It would be nice to not have to do this, but it seems as if there
+		 * is no easy way to avoid syscache/catcache misses in system catalog
+		 * indexes.  Just put up with somewhat less useful instrumentation.
+		 */
+		char	   *tp;			/* ptr to tuple data */
+
+		tp = (char *) itup + IndexInfoFindDataOffset(itup->t_info);
+		return psprintf("%lu", *((int64 *) tp));
+	}
+
+	natts = BTreeTupleGetNAtts(itup, rel);
+	natts = Min(natts, IndexRelationGetNumberOfKeyAttributes(rel));
+	itupdesc->natts = Min(orig_indnkeyatts, natts);
+	memset(&values, 0x00, sizeof(values));
+	memset(&isnull, 0x00, sizeof(isnull));
+	index_deform_tuple(itup, itupdesc, values, isnull);
+	rel->rd_index->indnkeyatts = natts;
+
+	/*
+	 * Since the regression tests should pass when the instrumentation patch
+	 * is applied, be prepared for BuildIndexValueDescription() to return NULL
+	 * due to security considerations.
+	 */
+	lkey_desc = BuildIndexValueDescription(rel, values, isnull);
+
+	/* Cleanup */
+	itupdesc->natts = orig_natts;
+	rel->rd_index->indnkeyatts = orig_indnkeyatts;
+
+	return lkey_desc;
+}
+
 
 /*
  *  _bt_check_natts() -- Verify tuple has expected number of attributes.
