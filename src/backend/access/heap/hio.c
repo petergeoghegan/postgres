1c4d51a2dcecc4f9624abdbdeecde6b8f9544296
Peter Geoghegan <pg@bowt.ie>
2020-08-16 12:07:53 -0700

    Tweak

--- src/backend/access/heap/hio.c
+++ src/backend/access/heap/hio.c
@@ -322,7 +322,7 @@ RelationGetBufferForTuple(Relation relation, Size len,
 						  BulkInsertState bistate,
 						  Buffer *vmbuffer, Buffer *vmbuffer_other)
 {
-	bool		use_fsm = !(options & HEAP_INSERT_SKIP_FSM);
+	bool		use_fsm = false;
 	Buffer		buffer = InvalidBuffer;
 	Page		page;
 	Size		pageFreeSpace = 0,
