56216cfcf2407d56f094de6af0dd5766ad235480
Peter Geoghegan <pg@bowt.ie>
2020-08-17 12:53:44 -0700

    Adjust prune threshold again.

--- src/backend/access/heap/pruneheap.c
+++ src/backend/access/heap/pruneheap.c
@@ -166,7 +166,7 @@ heap_page_prune_opt(Relation relation, Buffer buffer)
 	 */
 	minfree = RelationGetTargetPageFreeSpace(relation,
 											 HEAP_DEFAULT_FILLFACTOR);
-	minfree = Max(minfree, BLCKSZ / 10);
+	minfree = Max(minfree, BLCKSZ / 4);
 
 	if (PageIsFull(page) || PageGetHeapFreeSpace(page) < minfree)
 	{
