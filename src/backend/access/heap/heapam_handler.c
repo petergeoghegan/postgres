7d890fccd9838d6d0fb2cffce159cef448e06a74
Peter Geoghegan <pg@bowt.ie>
2020-08-23 12:57:06 -0700

    Tweak

--- src/backend/access/heap/heapam_handler.c
+++ src/backend/access/heap/heapam_handler.c
@@ -329,7 +329,7 @@ heapam_tuple_update(Relation relation, ItemPointer otid, TupleTableSlot *slot,
 	tuple->t_tableOid = slot->tts_tableOid;
 
 	result = heap_update(relation, otid, tuple, cid, crosscheck, wait,
-						 tmfd, lockmode);
+						 tmfd, lockmode, -1);
 	ItemPointerCopy(&tuple->t_self, &slot->tts_tid);
 
 	/*
