37de0b5b22d4ce43d7e36809d167282ea932d55a
Peter Geoghegan <pg@bowt.ie>
2021-03-28 20:55:55 -0700

    Bypass index vacuuming in some cases.
    Bypass index vacuuming in two cases: The case where there are so few
    dead tuples that index vacuuming seems unnecessary, and the case where
    the relfrozenxid of the table being vacuumed is dangerously far in the
    past.
    
    This commit add new GUC parameters vacuum_skip_index_age and
    vacuum_multixact_skip_index_age that specify age at which VACUUM
    should skip index cleanup to hurry finishing in order to
    advance relfrozenxid/relminmxid.
    
    After each index vacuuming (in non-parallel vacuum case), we check if
    the table's relfrozenxid/relminmxid are too old comparing those new
    GUC parameters. If so, we skip further index vacuuming within the
    vacuum operation.
    
    This behavior is intended to deal with the risk of XID wraparound, the
    default values are much higher, 1.8 billion.
    
    Although users can set those parameters, VACUUM will silently
    adjust the effective value more than 105% of
    autovacuum_freeze_max_age/autovacuum_multixact_freeze_max_age, so that
    only anti-wraparound autovacuuma and aggressive scan have a change to
    skip index vacuuming.

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -62,6 +62,8 @@ int			vacuum_freeze_min_age;
 int			vacuum_freeze_table_age;
 int			vacuum_multixact_freeze_min_age;
 int			vacuum_multixact_freeze_table_age;
+int			vacuum_skip_index_age;
+int			vacuum_multixact_skip_index_age;
 
 
 /* A few variables that don't seem worth passing around as parameters */
@@ -1132,6 +1134,65 @@ vacuum_set_xid_limits(Relation rel,
 	{
 		Assert(mxactFullScanLimit == NULL);
 	}
+}
+
+/*
+ * vacuum_xid_limit_emergency() -- Handle wraparound emergencies
+ *
+ * Input parameters are the target relation's relfrozenxid and relminmxid.
+ */
+bool
+vacuum_xid_limit_emergency(TransactionId relfrozenxid, MultiXactId relminmxid)
+{
+	TransactionId xid_skip_limit;
+	MultiXactId	  multi_skip_limit;
+	int			  skip_index_vacuum;
+
+	Assert(TransactionIdIsNormal(relfrozenxid));
+	Assert(MultiXactIdIsValid(relminmxid));
+
+	/*
+	 * Determine the index skipping age to use. In any case not less than
+	 * autovacuum_freeze_max_age * 1.05, so that VACUUM always does an
+	 * aggressive scan.
+	 */
+	skip_index_vacuum = Max(vacuum_skip_index_age, autovacuum_freeze_max_age * 1.05);
+
+	xid_skip_limit = ReadNextTransactionId() - skip_index_vacuum;
+	if (!TransactionIdIsNormal(xid_skip_limit))
+		xid_skip_limit = FirstNormalTransactionId;
+
+	if (TransactionIdIsNormal(relfrozenxid) &&
+		TransactionIdPrecedes(relfrozenxid, xid_skip_limit))
+	{
+		/* The table's relfrozenxid is too old */
+		return true;
+	}
+
+	/*
+	 * Similar to above, determine the index skipping age to use for multixact.
+	 * In any case not less than autovacuum_multixact_freeze_max_age * 1.05.
+	 */
+	skip_index_vacuum = Max(vacuum_multixact_skip_index_age,
+							autovacuum_multixact_freeze_max_age * 1.05);
+
+	/*
+	 * Compute the multixact age for which freezing is urgent.  This is
+	 * normally autovacuum_multixact_freeze_max_age, but may be less if we are
+	 * short of multixact member space.
+	 */
+	multi_skip_limit = ReadNextMultiXactId() - skip_index_vacuum;
+	if (multi_skip_limit < FirstMultiXactId)
+		multi_skip_limit = FirstMultiXactId;
+
+	if (MultiXactIdIsValid(relminmxid) &&
+		MultiXactIdPrecedes(relminmxid, multi_skip_limit))
+	{
+		/* The table's relminmxid is too old */
+		return true;
+	}
+
+	return false;
 }
 
 /*
