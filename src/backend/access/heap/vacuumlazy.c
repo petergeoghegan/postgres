1f642d5c07c8b76586489289891d419a153b65fa
Peter Geoghegan <pg@bowt.ie>
2021-03-30 19:43:06 -0700

    Truncate a pages' line pointer array when it has trailing unused ItemIds.
    This will allow reuse of what is effectively free space for data as well as
    new line pointers, instead of keeping it reserved for line pointers only.
    
    An additional benefit is that the HasFreeLinePointers hint-bit optimization
    now doesn't hint for free line pointers at the end of the array, slightly
    increasing the specificity of where the free lines are; and saving us from
    needing to search to the end of the array if all other entries are already
    filled.

--- src/backend/access/heap/vacuumlazy.c
+++ src/backend/access/heap/vacuumlazy.c
@@ -2183,7 +2183,8 @@ lazy_vacuum_heap_page(LVRelState *vacrel, BlockNumber blkno, Buffer buffer,
 
 	Assert(uncnt > 0);
 
-	PageSetHasFreeLinePointers(page);
+	/* Attempt to truncate line pointer array now */
+	PageShrinkEndUnused(page, uncnt);
 
 	/*
 	 * Mark buffer dirty before we write WAL.
