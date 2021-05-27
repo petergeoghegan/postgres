9ad3c20437e4285d7c0780b5be290b5ba76c1747
Peter Geoghegan <pg@bowt.ie>
2021-05-26 17:17:05 -0700

    Tweak

--- src/include/utils/rel.h
+++ src/include/utils/rel.h
@@ -307,6 +307,13 @@ typedef struct AutoVacOpts
 	float8		analyze_scale_factor;
 } AutoVacOpts;
 
+typedef enum HeapOptIndexCleanupMode
+{
+	HEAP_OPTION_INDEX_CLEANUP_AUTO,
+	HEAP_OPTION_INDEX_CLEANUP_ON,
+	HEAP_OPTION_INDEX_CLEANUP_OFF
+} HeapOptIndexCleanupMode;
+
 typedef struct StdRdOptions
 {
 	int32		vl_len_;		/* varlena header (do not touch directly!) */
@@ -316,7 +323,7 @@ typedef struct StdRdOptions
 	AutoVacOpts autovacuum;		/* autovacuum-related options */
 	bool		user_catalog_table; /* use as an additional catalog relation */
 	int			parallel_workers;	/* max number of parallel workers */
-	bool		vacuum_index_cleanup;	/* enables index vacuuming and cleanup */
+	HeapOptIndexCleanupMode		vacuum_index_cleanup;	/* enables index vacuuming and cleanup */
 	bool		vacuum_truncate;	/* enables vacuum to truncate a relation */
 } StdRdOptions;
 
