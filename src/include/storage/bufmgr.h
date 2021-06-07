f4f0bf17ac361d3323d31c765c27f5f75acc6b0d
Peter Geoghegan <pg@bowt.ie>
2021-06-07 09:40:34 -0700

    Experiment: Conditionally prune based on buffer dirty state

--- src/include/storage/bufmgr.h
+++ src/include/storage/bufmgr.h
@@ -214,6 +214,7 @@ extern void DropDatabaseBuffers(Oid dbid);
 	RelationGetNumberOfBlocksInFork(reln, MAIN_FORKNUM)
 
 extern bool BufferIsPermanent(Buffer buffer);
+extern bool BufferIsDirty(Buffer buffer);
 extern XLogRecPtr BufferGetLSNAtomic(Buffer buffer);
 
 #ifdef NOT_USED
