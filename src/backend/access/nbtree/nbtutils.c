b9935157c9f03c94c42c8f0588e48cde3e44c122
Peter Geoghegan <pg@bowt.ie>
2021-10-18 08:53:33 -0700

    Revert "Force v3 indexes for testing"
    This reverts commit 8b765b3409f8ca66fc310461d1ed2e15a6d22a35.

--- src/backend/access/nbtree/nbtutils.c
+++ src/backend/access/nbtree/nbtutils.c
@@ -135,11 +135,8 @@ _bt_mkscankey(Relation rel, IndexTuple itup)
 		_bt_metaversion(rel, &key->heapkeyspace, &key->allequalimage);
 	else
 	{
-		/*
-		 * XXX Continue to work with v4 indexes, while making sure new indexes are
-		 * v3 indexes
-		 */
-		key->heapkeyspace = false;
+		/* Utility statement callers can set these fields themselves */
+		key->heapkeyspace = true;
 		key->allequalimage = false;
 	}
 	key->anynullkeys = false;	/* initial assumption */
@@ -3818,9 +3815,6 @@ bool
 _bt_allequalimage(Relation rel, bool debugmessage)
 {
 	bool		allequalimage = true;
-
-	/* v3 indexes don't support allequalimage */
-	return false;
 
 	/* INCLUDE indexes can never support deduplication */
 	if (IndexRelationGetNumberOfAttributes(rel) !=
