ec731090e1baf17590b8be8be49db4093d84351c
Peter Geoghegan <pg@bowt.ie>
2022-07-17 18:50:13 -0700

    Show when autovacuum is insert driven in log.
    Teach the logging code controlled by log_autovacuum_min_duration to
    display more information about why its autovacuum worker was launched in
    the first place.  Show the user distinct output when the insert-based
    thresholds were the triggering condition.
    
    It's sometimes far from obvious why an autovacuum worker was launched
    without this new instrumentation.  It's useful to give feedback on the
    triggering condition, to guide tuning efforts by DBAs.
    
    Note that we consider insert-driven autovacuums to be a distinct
    category from antiwraparound autovacuums in all cases (though of course
    any VACUUM can end up as an aggressive VACUUM).

--- src/backend/postmaster/autovacuum.c
+++ src/backend/postmaster/autovacuum.c
@@ -329,12 +329,14 @@ static autovac_table *table_recheck_autovac(Oid relid, HTAB *table_toast_map,
 static void recheck_relation_needs_vacanalyze(Oid relid, AutoVacOpts *avopts,
 											  Form_pg_class classForm,
 											  int effective_multixact_freeze_max_age,
-											  bool *dovacuum, bool *doanalyze, bool *wraparound);
+											  bool *dovacuum, bool *doanalyze,
+											  bool *wraparound, bool *inserts);
 static void relation_needs_vacanalyze(Oid relid, AutoVacOpts *relopts,
 									  Form_pg_class classForm,
 									  PgStat_StatTabEntry *tabentry,
 									  int effective_multixact_freeze_max_age,
-									  bool *dovacuum, bool *doanalyze, bool *wraparound);
+									  bool *dovacuum, bool *doanalyze,
+									  bool *wraparound, bool *inserts);
 
 static void autovacuum_do_vac_analyze(autovac_table *tab,
 									  BufferAccessStrategy bstrategy);
@@ -2062,6 +2064,7 @@ do_autovacuum(void)
 		bool		dovacuum;
 		bool		doanalyze;
 		bool		wraparound;
+		bool		inserts;
 
 		if (classForm->relkind != RELKIND_RELATION &&
 			classForm->relkind != RELKIND_MATVIEW)
@@ -2102,7 +2105,8 @@ do_autovacuum(void)
 		/* Check if it needs vacuum or analyze */
 		relation_needs_vacanalyze(relid, relopts, classForm, tabentry,
 								  effective_multixact_freeze_max_age,
-								  &dovacuum, &doanalyze, &wraparound);
+								  &dovacuum, &doanalyze,
+								  &wraparound, &inserts);
 
 		/* Relations that need work are added to table_oids */
 		if (dovacuum || doanalyze)
@@ -2155,6 +2159,7 @@ do_autovacuum(void)
 		bool		dovacuum;
 		bool		doanalyze;
 		bool		wraparound;
+		bool		inserts;
 
 		/*
 		 * We cannot safely process other backends' temp tables, so skip 'em.
@@ -2185,7 +2190,8 @@ do_autovacuum(void)
 
 		relation_needs_vacanalyze(relid, relopts, classForm, tabentry,
 								  effective_multixact_freeze_max_age,
-								  &dovacuum, &doanalyze, &wraparound);
+								  &dovacuum, &doanalyze,
+								  &wraparound, &inserts);
 
 		/* ignore analyze for toast tables */
 		if (dovacuum)
@@ -2760,7 +2766,8 @@ table_recheck_autovac(Oid relid, HTAB *table_toast_map,
 	bool		dovacuum;
 	bool		doanalyze;
 	autovac_table *tab = NULL;
-	bool		wraparound;
+	bool		wraparound,
+				inserts;
 	AutoVacOpts *avopts;
 
 	/* fetch the relation's relcache entry */
@@ -2787,7 +2794,8 @@ table_recheck_autovac(Oid relid, HTAB *table_toast_map,
 
 	recheck_relation_needs_vacanalyze(relid, avopts, classForm,
 									  effective_multixact_freeze_max_age,
-									  &dovacuum, &doanalyze, &wraparound);
+									  &dovacuum, &doanalyze,
+									  &wraparound, &inserts);
 
 	/* OK, it needs something done */
 	if (doanalyze || dovacuum)
@@ -2868,6 +2876,7 @@ table_recheck_autovac(Oid relid, HTAB *table_toast_map,
 		tab->at_params.multixact_freeze_min_age = multixact_freeze_min_age;
 		tab->at_params.multixact_freeze_table_age = multixact_freeze_table_age;
 		tab->at_params.is_wraparound = wraparound;
+		tab->at_params.is_insert = inserts;
 		tab->at_params.log_min_duration = log_min_duration;
 		tab->at_vacuum_cost_limit = vac_cost_limit;
 		tab->at_vacuum_cost_delay = vac_cost_delay;
@@ -2903,7 +2912,8 @@ recheck_relation_needs_vacanalyze(Oid relid,
 								  int effective_multixact_freeze_max_age,
 								  bool *dovacuum,
 								  bool *doanalyze,
-								  bool *wraparound)
+								  bool *wraparound,
+								  bool *inserts)
 {
 	PgStat_StatTabEntry *tabentry;
 
@@ -2913,7 +2923,8 @@ recheck_relation_needs_vacanalyze(Oid relid,
 
 	relation_needs_vacanalyze(relid, avopts, classForm, tabentry,
 							  effective_multixact_freeze_max_age,
-							  dovacuum, doanalyze, wraparound);
+							  dovacuum, doanalyze,
+							  wraparound, inserts);
 
 	/* ignore ANALYZE for toast tables */
 	if (classForm->relkind == RELKIND_TOASTVALUE)
@@ -2966,7 +2977,8 @@ relation_needs_vacanalyze(Oid relid,
  /* output params below */
 						  bool *dovacuum,
 						  bool *doanalyze,
-						  bool *wraparound)
+						  bool *wraparound,
+						  bool *inserts)
 {
 	bool		force_vacuum;
 	bool		av_enabled;
@@ -3057,6 +3069,7 @@ relation_needs_vacanalyze(Oid relid,
 			MultiXactIdPrecedes(classForm->relminmxid, multiForceLimit);
 	}
 	*wraparound = force_vacuum;
+	*inserts = false;			/* for now */
 
 	/* User disabled it in pg_class.reloptions?  (But ignore if at risk) */
 	if (!av_enabled && !force_vacuum)
@@ -3103,8 +3116,9 @@ relation_needs_vacanalyze(Oid relid,
 				 vactuples, vacthresh, anltuples, anlthresh);
 
 		/* Determine if this table needs vacuum or analyze. */
-		*dovacuum = force_vacuum || (vactuples > vacthresh) ||
-			(vac_ins_base_thresh >= 0 && instuples > vacinsthresh);
+		*inserts = !force_vacuum && vac_ins_base_thresh >= 0 &&
+			instuples > vacinsthresh;
+		*dovacuum = force_vacuum || (vactuples > vacthresh) || *inserts;
 		*doanalyze = (anltuples > anlthresh);
 	}
 	else
