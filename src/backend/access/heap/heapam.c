d2523f52868b6dcc48437c1dffa843b33356df6f
Peter Geoghegan <pg@bowt.ie>
2022-07-18 14:35:44 -0700

    Add eager and lazy VM strategies to VACUUM.
    Acquire an in-memory immutable "snapshot" of the target rel's visibility
    map at the start of each VACUUM, and use the snapshot to determine when
    and how VACUUM will scan (or skip) heap pages (scanning strategy).  The
    data structure we a local copy of the visibility map at the start of
    VACUUM.  It spills to disk as required, though only with a larger table.
    
    VACUUM decides on its visibility map scanning and freezing strategies
    together, shortly before the first pass over the heap begins, since the
    concepts are closely related, and work in tandem.  Lazy scanning allows
    VACUUM to skip all-visible pages, while eager scanning allows VACUUM to
    advance relfrozenxid/relminmxid at the end of the VACUUM operation.
    
    This work, combined with recent work to add freezing strategies, results
    in VACUUM advancing relfrozenxid at a cadence that is barely influenced
    by autovacuum_freeze_max_age at all.  Now antiwraparound autovacuums
    will be far less common in practice.  Freezing now drives relfrozenxid,
    rather than relfrozenxid driving freezing.  Even tables that always use
    lazy freezing will have a decent chance of relfrozenxid advancement long
    before table age nears or exceeds autovacuum_freeze_max_age.
    
    This also lays the groundwork for completely removing aggressive mode
    VACUUMs in a later commit.  Scanning strategies now supersede the "early
    aggressive VACUUM" concept implemented by vacuum_freeze_table_age, which
    is now just a compatibility option (its new default of -1 is interpreted
    as "just use autovacuum_freeze_max_age").  Later work that makes the
    choice to wait for a cleanup lock depend entirely on individual page
    characteristics will decouple that "aggressive behavior" from the eager
    scanning strategy behavior (a behavior that's not really "aggressive" in
    any general sense, since it's chosen based on both costs and benefits).
    
    Also add explicit I/O prefetching of heap pages, which is controlled by
    maintenance_io_concurrency.  We prefetch at the point that the next
    block in line is requested by VACUUM.  Prefetching is under the direct
    control of the visibility map snapshot code, since VACUUM's vmsnap is
    now an authoritative guide to which pages VACUUM will scan.  VACUUM's
    final scanned_pages is "locked in" when it decides on scanning strategy
    (so scanned_pages is finalized before the first heap pass even begins).
    
    Prefetching should totally avoid the loss of performance that might
    otherwise result from removing SKIP_PAGES_THRESHOLD in this commit.
    SKIP_PAGES_THRESHOLD was intended to force OS readahead and encourage
    relfrozenxid advancement.  See commit bf136cf6 from around the time the
    visibility map first went in for full details.
    
    Also teach VACUUM to use scanned_pages (not rel_pages) to cap the size
    of the dead_items array.  This approach is strictly better, since there
    is no question of scanning any pages other than the precise set of pages
    already locked in by vmsnap by the time dead_items is allocated.
    
    Author: Peter Geoghegan <pg@bowt.ie>
    Reviewed-By: Andres Freund <andres@anarazel.de>
    Reviewed-By: Jeff Davis <pgsql@j-davis.com>
    Reviewed-By: John Naylor <john.naylor@enterprisedb.com>
    Discussion: https://postgr.es/m/CAH2-WzkFok_6EAHuK39GaW4FjEFQsY=3J0AAd6FXk93u-Xq3Fg@mail.gmail.com

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -6877,6 +6877,7 @@ heap_freeze_tuple(HeapTupleHeader tuple,
 	cutoffs.FreezeLimit = FreezeLimit;
 	cutoffs.MultiXactCutoff = MultiXactCutoff;
 	cutoffs.freeze_strategy_threshold = 0;
+	cutoffs.tableagefrac = 0;
 
 	pagefrz.freeze_required = true;
 	pagefrz.FreezePageRelfrozenXid = FreezeLimit;
