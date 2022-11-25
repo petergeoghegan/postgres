57cac4d59672528e48d3e772fde0a91400c03480
Peter Geoghegan <pg@bowt.ie>
2022-11-25 11:23:20 -0800

    Add "table age" trigger concept to autovacuum.
    Teach autovacuum.c to launch "table age" autovacuums at the same point
    that it previously triggered antiwraparound autovacuums.  Antiwraparound
    autovacuums are retained, but are only used as a true option of last
    resort, when regular autovacuum has presumably tried and failed to
    advance relfrozenxid (likely because the auto-cancel behavior kept
    cancelling regular autovacuums triggered based on table age).  The
    special auto-cancellation behavior applied by antiwraparound autovacuums
    is known to cause problems in the field, so it makes sense to avoid it,
    at least until the point where it starts to look like a proportionate
    response.
    
    Also add new instrumentation that lists a triggering condition in the
    server log whenever an autovacuum is logged.  This reports "table age"
    as the triggering criteria when regular (not antiwraparound) autovacuum
    runs because we need to advance the table's age.  In other cases it will
    report an autovacuum was launched due to the tuple insert thresholds or
    the dead tuple thresholds.  Note that pg_stat_activity doesn't have any
    special instrumentation for regular autovacuums that happen to have been
    triggered based on table age (since it really is just another form of
    autovacuum, and follows exactly the same rules in vacuumlazy.c and in
    proc.c).
    
    Author: Peter Geoghegan <pg@bowt.ie>
    Reviewed-By: Jeff Davis <pgsql@j-davis.com>
    Discussion: https://postgr.es/m/CAH2-Wz=S-R_2rO49Hm94Nuvhu9_twRGbTm6uwDRmRu-Sqn_t3w@mail.gmail.com

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -263,8 +263,9 @@ ExecVacuum(ParseState *pstate, VacuumStmt *vacstmt, bool isTopLevel)
 		params.multixact_freeze_table_age = -1;
 	}
 
-	/* user-invoked vacuum is never "for wraparound" */
+	/* user-invoked vacuum never uses these autovacuum-only flags */
 	params.is_wraparound = false;
+	params.trigger = AUTOVACUUM_NONE;
 
 	/* user-invoked vacuum uses VACOPT_VERBOSE instead of log_min_duration */
 	params.log_min_duration = -1;
@@ -1826,7 +1827,8 @@ vacuum_rel(Oid relid, RangeVar *relation, VacuumParams *params)
 		 *
 		 * We also set the VACUUM_FOR_WRAPAROUND flag, which is passed down by
 		 * autovacuum; it's used to avoid canceling a vacuum that was invoked
-		 * in an emergency.
+		 * because no earlier vacuum (in particular no earlier "table age"
+		 * autovacuum) ran and advanced relfrozenxid/relminmxid.
 		 *
 		 * Note: these flags remain set until CommitTransaction or
 		 * AbortTransaction.  We don't want to clear them until we reset
@@ -1838,7 +1840,10 @@ vacuum_rel(Oid relid, RangeVar *relation, VacuumParams *params)
 		LWLockAcquire(ProcArrayLock, LW_EXCLUSIVE);
 		MyProc->statusFlags |= PROC_IN_VACUUM;
 		if (params->is_wraparound)
+		{
+			Assert(params->trigger == AUTOVACUUM_TABLEAGE);
 			MyProc->statusFlags |= PROC_VACUUM_FOR_WRAPAROUND;
+		}
 		ProcGlobal->statusFlags[MyProc->pgxactoff] = MyProc->statusFlags;
 		LWLockRelease(ProcArrayLock);
 	}
