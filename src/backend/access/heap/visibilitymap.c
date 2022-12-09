ed91301fe2c42f5dc405c173bc657cae7473d57b
Peter Geoghegan <pg@bowt.ie>
2022-12-09 14:29:48 -0800

    Revert "Add temp vm snapshot debug infrastructure."
    This reverts commit b2010792c1e4dd5036a75f3a345220940404b5d1.

--- src/backend/access/heap/visibilitymap.c
+++ src/backend/access/heap/visibilitymap.c
@@ -109,8 +109,6 @@
 
 /*#define TRACE_VISIBILITYMAP */
 
-#define DEBUG_ELOG WARNING
-
 /*
  * Size of the bitmap on each visibility map page, in bytes. There's no
  * extra headers, so the whole page minus the standard page header is
@@ -222,7 +220,7 @@ visibilitymap_clear(Relation rel, BlockNumber heapBlk, Buffer vmbuf, uint8 flags
 	Assert(flags & VISIBILITYMAP_VALID_BITS);
 
 #ifdef TRACE_VISIBILITYMAP
-	elog(DEBUG_ELOG, "vm_clear %s %d", RelationGetRelationName(rel), heapBlk);
+	elog(DEBUG1, "vm_clear %s %d", RelationGetRelationName(rel), heapBlk);
 #endif
 
 	if (!BufferIsValid(vmbuf) || BufferGetBlockNumber(vmbuf) != mapBlock)
@@ -325,7 +323,7 @@ visibilitymap_set(Relation rel, BlockNumber heapBlk, Buffer heapBuf,
 	uint8	   *map;
 
 #ifdef TRACE_VISIBILITYMAP
-	elog(DEBUG2, "vm_set %s %d", RelationGetRelationName(rel), heapBlk);
+	elog(DEBUG1, "vm_set %s %d", RelationGetRelationName(rel), heapBlk);
 #endif
 
 	Assert(InRecovery || XLogRecPtrIsInvalid(recptr));
@@ -415,7 +413,7 @@ visibilitymap_get_status(Relation rel, BlockNumber heapBlk, Buffer *vmbuf)
 	uint8		result;
 
 #ifdef TRACE_VISIBILITYMAP
-	elog(DEBUG_ELOG, "vm_get_status %s %d", RelationGetRelationName(rel), heapBlk);
+	elog(DEBUG1, "vm_get_status %s %d", RelationGetRelationName(rel), heapBlk);
 #endif
 
 	/* Reuse the old pinned buffer if possible */
@@ -474,7 +472,7 @@ visibilitymap_snap_acquire(Relation rel, BlockNumber rel_pages,
 	vmsnapshot *vmsnap;
 
 #ifdef TRACE_VISIBILITYMAP
-	elog(DEBUG_ELOG, "visibilitymap_snap_acquire %s %u",
+	elog(DEBUG1, "visibilitymap_snap_acquire %s %u",
 		 RelationGetRelationName(rel), rel_pages);
 #endif
 
@@ -634,7 +632,7 @@ visibilitymap_snap_strategy(vmsnapshot *vmsnap, vmstrategy strat)
 	vmsnap->scanned_pages_to_prefetch = vmsnap->scanned_pages_to_return;
 
 #ifdef TRACE_VISIBILITYMAP
-	elog(DEBUG_ELOG, "visibilitymap_snap_strategy %s %d %u",
+	elog(DEBUG1, "visibilitymap_snap_strategy %s %d %u",
 		 RelationGetRelationName(vmsnap->rel), (int) strat,
 		 vmsnap->scanned_pages_to_return);
 #endif
@@ -655,7 +653,6 @@ visibilitymap_snap_strategy(vmsnapshot *vmsnap, vmstrategy strat)
 		{
 			BlockNumber block = vmsnap->staged[i].block;
 
-			elog(DEBUG_ELOG, "init prefetch block: %u", block);
 			PrefetchBuffer(vmsnap->rel, MAIN_FORKNUM, block);
 		}
 
@@ -713,8 +710,6 @@ visibilitymap_snap_next(vmsnapshot *vmsnap, bool *allvisible)
 		{
 			vmsnapblock prefetch = vmsnap->staged[vmsnap->next_block_prefetch_idx++];
 
-			elog(DEBUG_ELOG, "scan prefetch block: %u (just about to return next block %u to vacuum)",
-				 prefetch.block, next_block_to_scan);
 			Assert(vmsnap->scanned_pages_to_prefetch > 0);
 
 			PrefetchBuffer(vmsnap->rel, MAIN_FORKNUM, prefetch.block);
@@ -722,8 +717,6 @@ visibilitymap_snap_next(vmsnapshot *vmsnap, bool *allvisible)
 		}
 		else
 		{
-			elog(DEBUG_ELOG, "return next block %u to vacuum without prefetch",
-				 next_block_to_scan);
 			Assert(vmsnap->scanned_pages_to_prefetch == 0);
 		}
 
@@ -745,7 +738,7 @@ visibilitymap_snap_next(vmsnapshot *vmsnap, bool *allvisible)
 	}
 
 #ifdef TRACE_VISIBILITYMAP
-	elog(DEBUG_ELOG, "visibilitymap_snap_next %s %u",
+	elog(DEBUG1, "visibilitymap_snap_next %s %u",
 		 RelationGetRelationName(vmsnap->rel), next_block_to_scan);
 #endif
 
@@ -850,7 +843,7 @@ visibilitymap_prepare_truncate(Relation rel, BlockNumber nheapblocks)
 	uint8		truncOffset = HEAPBLK_TO_OFFSET(nheapblocks);
 
 #ifdef TRACE_VISIBILITYMAP
-	elog(DEBUG_ELOG, "vm_truncate %s %d", RelationGetRelationName(rel), nheapblocks);
+	elog(DEBUG1, "vm_truncate %s %d", RelationGetRelationName(rel), nheapblocks);
 #endif
 
 	/*
@@ -1149,7 +1142,7 @@ vm_snap_get_status(vmsnapshot *vmsnap, BlockNumber heapBlk)
 	uint8		mapOffset = HEAPBLK_TO_OFFSET(heapBlk);
 
 #ifdef TRACE_VISIBILITYMAP
-	elog(DEBUG_ELOG, "vm_snap_get_status %u", heapBlk);
+	elog(DEBUG1, "vm_snap_get_status %u", heapBlk);
 #endif
 
 	/*
