1eb41411ac78e3eea2876ef0b1cf2d7c445e230f
Peter Geoghegan <pg@bowt.ie>
2024-02-25 18:09:00 -0500

    Skip count instrumentation.

--- src/backend/access/nbtree/nbtutils.c
+++ src/backend/access/nbtree/nbtutils.c
@@ -3912,7 +3912,10 @@ _bt_check_compare(ScanDirection dir, BTScanOpaque so,
 		if (prechecked &&
 			(requiredSameDir || (requiredOppositeDirOnly && firstmatch)) &&
 			!(key->sk_flags & SK_ROW_HEADER))
+		{
+			so->skipsamecount++;
 			continue;
+		}
 
 		if (key->sk_attno > tupnatts)
 		{
@@ -4069,6 +4072,8 @@ _bt_check_compare(ScanDirection dir, BTScanOpaque so,
 			 */
 			return false;
 		}
+		if (requiredOppositeDirOnly && firstmatch)
+			so->skipoppocount++;
 	}
 
 	if (so->log_btree_verbosity >= 3)
