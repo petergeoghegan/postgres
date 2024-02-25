1345bf9ffac5848ba0413020dd8a96b41c616a30
Peter Geoghegan <pg@bowt.ie>
2024-02-25 18:09:00 -0500

    Skip count instrumentation.

--- src/backend/access/nbtree/nbtree.c
+++ src/backend/access/nbtree/nbtree.c
@@ -381,6 +381,8 @@ btbeginscan(Relation rel, int nkeys, int norderbys)
 	so->arrayKeys = NULL;
 	so->orderProcs = NULL;
 	so->arrayContext = NULL;
+	so->skipsamecount = 0;
+	so->skipoppocount = 0;
 
 	so->killedItems = NULL;		/* until needed */
 	so->numKilled = 0;
@@ -502,6 +504,11 @@ btendscan(IndexScanDesc scan)
 		ereport(LOG,
 				(errmsg_internal("%s", so->debugstr.data)));
 	}
+
+	if (so->numArrayKeys && (so->skipsamecount > 0 /* || so->skipoppocount > 0 */))
+		elog(WARNING, "rel %s, skipsamecount: %d, skipoppocount: %d",
+			 RelationGetRelationName(scan->indexRelation),
+			 so->skipsamecount, so->skipoppocount);
 
 	/* we aren't holding any read locks, but gotta drop the pins */
 	if (BTScanPosIsValid(so->currPos))
--- src/backend/access/nbtree/nbtutils.c
+++ src/backend/access/nbtree/nbtutils.c
@@ -4004,7 +4004,10 @@ _bt_check_compare(IndexScanDesc scan, ScanDirection dir,
 		if (prechecked &&
 			(requiredSameDir || (requiredOppositeDirOnly && firstmatch)) &&
 			!(key->sk_flags & SK_ROW_HEADER))
+		{
+			so->skipsamecount++;
 			continue;
+		}
 
 		if (key->sk_attno > tupnatts)
 		{
@@ -4165,6 +4168,8 @@ _bt_check_compare(IndexScanDesc scan, ScanDirection dir,
 			 */
 			return false;
 		}
+		if (requiredOppositeDirOnly && firstmatch)
+			so->skipoppocount++;
 	}
 
 	if (so->log_btree_verbosity >= 3)
