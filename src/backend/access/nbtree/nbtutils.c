9367e7d7330ed149e68518b3a6c321b72231f9d0
Peter Geoghegan <pg@bowt.ie>
2024-03-15 18:26:49 -0400

    Revert "Skip count instrumentation."
    This reverts commit 205ea0bff807c6765b2c706eef868ae1342fbcd3.

--- src/backend/access/nbtree/nbtutils.c
+++ src/backend/access/nbtree/nbtutils.c
@@ -4032,10 +4032,7 @@ _bt_check_compare(IndexScanDesc scan, ScanDirection dir,
 		if (prechecked &&
 			(requiredSameDir || (requiredOppositeDirOnly && firstmatch)) &&
 			!(key->sk_flags & SK_ROW_HEADER))
-		{
-			so->skipsamecount++;
 			continue;
-		}
 
 		if (key->sk_attno > tupnatts)
 		{
@@ -4196,8 +4193,6 @@ _bt_check_compare(IndexScanDesc scan, ScanDirection dir,
 			 */
 			return false;
 		}
-		if (requiredOppositeDirOnly && firstmatch)
-			so->skipoppocount++;
 	}
 
 	if (so->log_btree_verbosity >= 3)
