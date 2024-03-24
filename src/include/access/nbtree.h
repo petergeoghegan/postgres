5f4a27e556414f9afb69fd9cf8ebbf6b53dabcca
Peter Geoghegan <pg@bowt.ie>
2024-03-24 19:20:39 -0400

    Revert "Skip count instrumentation."
    This reverts commit 2d25a5ae8d6c6d490564251e02beaebede8c3b30.

--- src/include/access/nbtree.h
+++ src/include/access/nbtree.h
@@ -1053,8 +1053,6 @@ typedef struct BTScanOpaqueData
 	int			npages;
 	int			nsearches;
 	MemoryContext arrayContext; /* scan-lifespan context for array data */
-	int			skipsamecount;
-	int			skipoppocount;
 
 	/* info about killed items if any (killedItems is NULL if never used) */
 	int		   *killedItems;	/* currPos.items indexes of killed items */
