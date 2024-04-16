75843cff02a38e8f267fdfc24dc33f06bba20efd
Peter Geoghegan <pg@bowt.ie>
2024-04-16 17:44:22 -0400

    New test suite for skip scan project

--- src/test/regress/sql/small_dynamic_saop_advancement.sql
+++ src/test/regress/sql/small_dynamic_saop_advancement.sql
@@ -3,23 +3,20 @@ set enable_seqscan to off;
 set enable_indexscan to off;
 set enable_bitmapscan to on;
 
-select set_config((select coalesce((select name from pg_settings where name = 'log_btree_verbosity'), 'commit_siblings')), '0', false);
+select set_config((select coalesce((select name from pg_settings where name = 'log_btree_verbosity'), 'commit_siblings')), '2', false);
 
 set client_min_messages=error;
-drop table if exists multi_test_small;
-drop table if exists duplicate_test_small;
+drop table if exists multi_test;
 reset client_min_messages;
 
-create unlogged table multi_test_small(
+create unlogged table multi_test(
   a int,
   b int
 );
-create unlogged table duplicate_test_small(dup int4);
 
-create index multi_test_small_idx on multi_test_small(a, b);
-create index on duplicate_test_small (dup);
+create index multi_test_idx on multi_test(a, b);
 
-insert into multi_test_small
+insert into multi_test
 select
   j,
   case when i < 14 then
@@ -34,355 +31,106 @@ order by
   j,
   i;
 
-insert into duplicate_test_small
-select val from generate_series(1, 18) val,
-                generate_series(1,1000) dups_per_val;
+vacuum analyze multi_test;
 
-vacuum (freeze,analyze) multi_test_small;
-vacuum (freeze,analyze) duplicate_test_small; -- Be tidy
+-- Looks like this now:
+--
+-- ┌───┬───────┬───────┬────────┬────────┬────────────┬───────┬───────┬───────────────────┬─────────┬───────────┬──────────────┐
+-- │ i │ blkno │ flags │ nhtids │ nhblks │ ndeadhblks │ nlive │ ndead │ nhtidschecksimple │ avgsize │ freespace │   highkey    │
+-- ├───┼───────┼───────┼────────┼────────┼────────────┼───────┼───────┼───────────────────┼─────────┼───────────┼──────────────┤
+-- │ 1 │     1 │     1 │    854 │      4 │          0 │   123 │     0 │                 0 │      55 │       808 │ (a, b)=(62)  │
+-- │ 2 │     2 │     1 │    854 │      5 │          0 │   123 │     0 │                 0 │      55 │       808 │ (a, b)=(123) │
+-- │ 3 │     4 │     1 │    854 │      5 │          0 │   123 │     0 │                 0 │      55 │       808 │ (a, b)=(184) │
+-- │ 4 │     5 │     1 │    854 │      5 │          0 │   123 │     0 │                 0 │      55 │       808 │ (a, b)=(245) │
+-- │ 5 │     6 │     1 │    854 │      4 │          0 │   123 │     0 │                 0 │      55 │       808 │ (a, b)=(306) │
+-- │ 6 │     7 │     1 │    854 │      5 │          0 │   123 │     0 │                 0 │      55 │       808 │ (a, b)=(367) │
+-- │ 7 │     8 │     1 │    476 │      3 │          0 │    80 │     0 │                 0 │      49 │     3,908 │ ∅            │
+-- └───┴───────┴───────┴────────┴────────┴────────────┴───────┴───────┴───────────────────┴─────────┴───────────┴──────────────┘
+--
+-----------------------------------------------------------------------
 
--- Simple contradictory
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a = 181;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 0 buffer hits (contradictory qual)
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a = 181;
+-- Bitmap index scan:
+set enable_bitmapscan to on;
+set enable_indexonlyscan to off;
+set enable_indexscan to off;
 
--- Simple contradictory, but flip order
-select *
-from multi_test_small
-where
-  a = 181
-  and a in (1, 99, 182, 183, 184);
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 0 buffer hits (contradictory qual)
-select *
-from multi_test_small
-where
-  a = 181
-  and a in (1, 99, 182, 183, 184);
+select * from multi_test where a in (183) and b in (1,2,3,4,5,6,7,8,9,10,11,12);
+select * from multi_test where a in (123, 182, 183) and b in (1,2);
 
--- Simple redundant
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a = 182;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- only 2 buffer hits for '182' (redundant qual)
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a = 182;
+set client_min_messages=error;
+drop table if exists wisconsin;
+reset client_min_messages;
 
--- Simple redundant, but flip order
-select *
-from multi_test_small
-where
-  a = 182
-  and a in (1, 99, 182, 183, 184);
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- only 2 buffer hits for '182' (redundant qual)
-select *
-from multi_test_small
-where
-  a = 182
-  and a in (1, 99, 182, 183, 184);
+create unlogged table wisconsin
+(
+unique1 int4,
+unique2 int4,
+two int4,
+four int4,
+ten int4,
+twenty int4,
+onepercent int4,
+tenpercent int4,
+twentypercent int4,
+fiftypercent int4,
+unique3 int4,
+evenonepercent int4,
+oddonepercent int4,
+stringu1 text,
+stringu2 text,
+string4 text
+);
 
----------------------------------
--- '>' operator/strategy tests --
----------------------------------
+\getenv abs_srcdir PG_ABS_SRCDIR
+\set filename :abs_srcdir '/data/wisconsin.csv'
+COPY wisconsin FROM :'filename' with (format csv, encoding 'win1252', header false, null $$$$, quote $$'$$); -- Fix the syntax highlighting: '
 
--- Simple > contradictory
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a > 184;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 0 buffer hits (contradictory qual)
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a > 184;
+-- Ten:
+create index ten_idx on wisconsin (ten, unique1);
 
--- Simple > contradictory, but flip order
-select *
-from multi_test_small
-where
-  a > 184
-  and a in (1, 99, 182, 183, 184);
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 0 buffer hits (contradictory qual)
-select *
-from multi_test_small
-where
-  a > 184
-  and a in (1, 99, 182, 183, 184);
+-- Range instead of skip attribute on "ten":
+select ten, unique1 from wisconsin where ten between -10000 and 4 and unique1 = 5555;
+EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- master 412 hits
+select ten, unique1 from wisconsin where ten between -10000 and 4 and unique1 = 5555;
 
--- Simple > redundant
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a > 183;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- only 2 buffer hits for '184' (redundant qual)
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a > 183;
+-----------------------
+-- tenk1 test cases  --
+-----------------------
+set client_min_messages=error;
+drop table if exists tenk1_skipscan;
+reset client_min_messages;
+\getenv abs_srcdir PG_ABS_SRCDIR
+CREATE UNLOGGED TABLE tenk1_skipscan (
+	unique1		int4,
+	unique2		int4,
+	two			int4,
+	four		int4,
+	ten			int4,
+	twenty		int4,
+	hundred		int4,
+	thousand	int4,
+	twothousand	int4,
+	fivethous	int4,
+	tenthous	int4,
+	odd			int4,
+	even		int4,
+	stringu1	name,
+	stringu2	name,
+	string4		name
+);
+ALTER TABLE tenk1_skipscan SET (autovacuum_enabled=off);
 
--- Simple > redundant, but flip order
-select *
-from multi_test_small
-where
-  a > 183
-  and a in (1, 99, 182, 183, 184);
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- only 2 buffer hits for '184' (redundant qual)
-select *
-from multi_test_small
-where
-  a > 183
-  and a in (1, 99, 182, 183, 184);
+\set filename :abs_srcdir '/data/tenk.data'
+COPY tenk1_skipscan FROM :'filename';
+VACUUM ANALYZE tenk1_skipscan;
 
-----------------------------------
--- '>=' operator/strategy tests --
-----------------------------------
+CREATE INDEX tenk1_skipscan_four_unique1 ON tenk1_skipscan (four, unique1);
 
--- Simple >= contradictory
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a >= 185;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 0 buffer hits (contradictory qual)
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a >= 185;
+prepare tenk1_four_skipscan as
+SELECT hundred, unique1 FROM tenk1_skipscan
+ WHERE unique1 = 444;
 
--- Simple >= contradictory, but flip order
-select *
-from multi_test_small
-where
-  a >= 185
-  and a in (1, 99, 182, 183, 184);
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 0 buffer hits (contradictory qual)
-select *
-from multi_test_small
-where
-  a >= 185
-  and a in (1, 99, 182, 183, 184);
-
--- Simple >= redundant
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a >= 184;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- only 2 buffer hits for '184' (redundant qual)
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a >= 184;
-
--- Simple >= redundant, but flip order
-select *
-from multi_test_small
-where
-  a >= 184
-  and a in (1, 99, 182, 183, 184);
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- only 2 buffer hits for '184' (redundant qual)
-select *
-from multi_test_small
-where
-  a >= 184
-  and a in (1, 99, 182, 183, 184);
-
----------------------------------
--- '<' operator/strategy tests --
----------------------------------
-
--- Simple < contradictory
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a < 1;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 0 buffer hits (contradictory qual)
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a < 1;
-
--- Simple < contradictory, but flip order
-select *
-from multi_test_small
-where
-  a < 1
-  and a in (1, 99, 182, 183, 184);
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 0 buffer hits (contradictory qual)
-select *
-from multi_test_small
-where
-  a < 1
-  and a in (1, 99, 182, 183, 184);
-
--- Simple < redundant
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a < 2;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- only 2 buffer hits for '1' (redundant qual)
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a < 2;
-
--- Simple < redundant, but flip order
-select *
-from multi_test_small
-where
-  a < 2
-  and a in (1, 99, 182, 183, 184);
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- only 2 buffer hits for '1' (redundant qual)
-select *
-from multi_test_small
-where
-  a < 2
-  and a in (1, 99, 182, 183, 184);
-
-----------------------------------
--- '<=' operator/strategy tests --
-----------------------------------
-
--- Simple <= contradictory
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a <= 0;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 0 buffer hits (contradictory qual)
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a <= 0;
-
--- Simple <= contradictory, but flip order
-select *
-from multi_test_small
-where
-  a <= 0
-  and a in (1, 99, 182, 183, 184);
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 0 buffer hits (contradictory qual)
-select *
-from multi_test_small
-where
-  a <= 0
-  and a in (1, 99, 182, 183, 184);
-
--- Simple <= redundant
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a <= 1;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- only 2 buffer hits for '1' (redundant qual)
-select *
-from multi_test_small
-where
-  a in (1, 99, 182, 183, 184)
-  and a <= 1;
-
--- Simple <= redundant, but flip order
-select *
-from multi_test_small
-where
-  a <= 1
-  and a in (1, 99, 182, 183, 184);
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- only 2 buffer hits for '1' (redundant qual)
-select *
-from multi_test_small
-where
-  a <= 1
-  and a in (1, 99, 182, 183, 184);
-
--- Duplicate test, index-only scan
-set enable_bitmapscan to off;
-set enable_indexonlyscan to on;
-
--- Warm up test
-select count(*), dup from duplicate_test_small
-where dup = any (array[( select array_agg(val) from generate_series(1, 40) val)])
-group by dup order by dup;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 20 hits vs 81 on master
-select count(*), dup from duplicate_test_small
-where dup = any (array[( select array_agg(val) from generate_series(1, 40) val)])
-group by dup order by dup;
-
--- Now real tests begin
-
-select count(*), dup from duplicate_test_small
-where dup = any (array[( select array_agg(val) from generate_series(1, 40) val)])
-and dup > 8 and dup < 11
-group by dup order by dup;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 4 hits (include one vm hit)
-select count(*), dup from duplicate_test_small
-where dup = any (array[( select array_agg(val) from generate_series(1, 40) val)])
-and dup > 8 and dup < 11
-group by dup order by dup;
-
--- Variant #1 (changes the order, not the true meaning):
-select count(*), dup from duplicate_test_small
-where
-dup > 8 and
-dup = any(array[( select array_agg(val) from generate_series(1, 40) val)]) and
-dup < 11
-group by dup order by dup;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 4 hits (include one vm hit)
-select count(*), dup from duplicate_test_small
-where
-dup > 8 and
-dup = any(array[( select array_agg(val) from generate_series(1, 40) val)]) and
-dup < 11
-group by dup order by dup;
-
--- Variant #2 (changes the order, not the true meaning):
-select count(*), dup from duplicate_test_small
-where
-dup > 8 and
-dup < 11 and
-dup = any(array[( select array_agg(val) from generate_series(1, 40) val)])
-group by dup order by dup;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 4 hits (include one vm hit)
-select count(*), dup from duplicate_test_small
-where
-dup > 8 and
-dup < 11 and
-dup = any(array[( select array_agg(val) from generate_series(1, 40) val)])
-group by dup order by dup;
-
--- Variant #3 (changes the order, not the true meaning):
-select count(*), dup from duplicate_test_small
-where
-dup < 11 and
-dup > 8 and
-dup = any(array[( select array_agg(val) from generate_series(1, 40) val)])
-group by dup order by dup;
-EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- 4 hits (include one vm hit)
-select count(*), dup from duplicate_test_small
-where
-dup < 11 and
-dup > 8 and
-dup = any(array[( select array_agg(val) from generate_series(1, 40) val)])
-group by dup order by dup;
+execute tenk1_four_skipscan;
+EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF) -- master 30 hits
+execute tenk1_four_skipscan;
+deallocate tenk1_four_skipscan;
