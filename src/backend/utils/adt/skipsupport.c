97032cc11192ddc5a44ae30cdc97d58c9fca3c3d
Peter Geoghegan <pg@bowt.ie>
2024-06-22 21:14:38 -0400

    Remove

--- src/backend/utils/adt/skipsupport.c
+++ src/backend/utils/adt/skipsupport.c
@@ -27,21 +27,6 @@ skipsupport_setup_callbacks(Oid opfamily, Oid opcintype, bool reverse,
 {
 	Oid			skipSupportFunction;
 
-	/*
-	 * XXX Short-term measure, to paper-over a known test failure caused by
-	 * cross-type preprocessing bug (needed for 32-bit CI to pass).
-	 *
-	 * This is required to get the create_index regression test to pass when
-	 * !USE_FLOAT8_BYVAL.  Once _bt_skip_scankey_preprocess is taught to set
-	 * low_value and high_value in a way that accounts for cross-type
-	 * differences within a B-Tree opfamily (by fixing its FIXME item), we
-	 * won't need to do this anymore.
-	 */
-#ifndef USE_FLOAT8_BYVAL
-	if (opcintype == INT8OID)
-		return false;
-#endif
-
 	/* Look for a skip support function */
 	skipSupportFunction = get_opfamily_proc(opfamily, opcintype, opcintype,
 											BTSKIPSUPPORT_PROC);
