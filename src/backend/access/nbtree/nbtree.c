c0b4a2f79be7214db2c1db39fe1c711c09bd1963
Peter Geoghegan <pg@bowt.ie>
2024-07-09 14:56:03 -0400

    DEBUG: make _bt_advance_array_keys invariant violations into errors
    Promotes the precondition and postcondition assertions within
    _bt_advance_array_keys into "can't happen" elog ERRORs.  Also detects
    repeat accesses to the same leaf page, which aren't expected (barring
    certain limited edge cases, which are accounted for here).

--- src/backend/access/nbtree/nbtree.c
+++ src/backend/access/nbtree/nbtree.c
@@ -390,6 +390,7 @@ btbeginscan(Relation rel, int nkeys, int norderbys)
 	else
 		so->keyData = NULL;
 
+	so->bmsPages = NULL;
 	so->needPrimScan = false;
 	so->scanBehind = false;
 	so->oppositeDirCheck = false;
@@ -451,6 +452,8 @@ btrescan(IndexScanDesc scan, ScanKey scankey, int nscankeys,
 			_bt_killitems(scan);
 		BTScanPosUnpinIfPinned(so->currPos);
 		BTScanPosInvalidate(so->currPos);
+		bms_free(so->bmsPages);
+		so->bmsPages = NULL;
 		if (so->log_btree_verbosity)
 			appendStringInfo(&so->debugstr, "btrescan: BTScanPosInvalidate() called for currPos\n");
 	}
@@ -635,6 +638,8 @@ btrestrpos(IndexScanDesc scan)
 			{
 				_bt_start_array_keys(scan, so->currPos.dir);
 				so->needPrimScan = false;
+				bms_free(so->bmsPages);
+				so->bmsPages = NULL;
 			}
 		}
 		else
