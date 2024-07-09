b2bd0487da275a06f622bfa5d96886f324571acd
Peter Geoghegan <pg@bowt.ie>
2024-07-09 14:56:03 -0400

    DEBUG: make _bt_advance_array_keys invariant violations into errors
    Promotes the precondition and postcondition assertions within
    _bt_advance_array_keys into "can't happen" elog ERRORs.  Also detects
    repeat accesses to the same leaf page, which aren't expected (barring
    certain limited edge cases, which are accounted for here).

--- src/backend/access/nbtree/nbtree.c
+++ src/backend/access/nbtree/nbtree.c
@@ -389,6 +389,7 @@ btbeginscan(Relation rel, int nkeys, int norderbys)
 	else
 		so->keyData = NULL;
 
+	so->bmsPages = NULL;
 	so->needPrimScan = false;
 	so->scanBehind = false;
 	so->oppositeDirCheck = false;
@@ -450,6 +451,8 @@ btrescan(IndexScanDesc scan, ScanKey scankey, int nscankeys,
 			_bt_killitems(scan);
 		BTScanPosUnpinIfPinned(so->currPos);
 		BTScanPosInvalidate(so->currPos);
+		bms_free(so->bmsPages);
+		so->bmsPages = NULL;
 		if (so->log_btree_verbosity)
 			appendStringInfo(&so->debugstr, "btrescan: BTScanPosInvalidate() called for currPos\n");
 	}
@@ -633,6 +636,8 @@ btrestrpos(IndexScanDesc scan)
 			{
 				_bt_start_array_keys(scan, so->currPos.dir);
 				so->needPrimScan = false;
+				bms_free(so->bmsPages);
+				so->bmsPages = NULL;
 			}
 		}
 		else
--- src/backend/access/nbtree/nbtutils.c
+++ src/backend/access/nbtree/nbtutils.c
@@ -3206,8 +3206,19 @@ _bt_advance_array_keys(IndexScanDesc scan, BTReadPageState *pstate,
 		/*
 		 * Precondition array state assertion
 		 */
-		Assert(!_bt_tuple_before_array_skeys(scan, dir, tuple, tupdesc,
-											 tupnatts, false, 0, NULL));
+		if (!(!_bt_tuple_before_array_skeys(scan, dir, tuple, tupdesc,
+											tupnatts, false, 0, NULL)))
+		{
+			char	   *pitup;
+
+			pitup = _nbtree_print_itup(tuple, rel);
+
+			elog(ERROR, "pitup: %s\n\n %s", pitup, so->debugstr.data);
+
+			if (pitup)
+				pfree(pitup);
+
+		}
 
 		so->scanBehind = so->oppositeDirCheck = false;	/* reset */
 
@@ -3740,9 +3751,20 @@ _bt_advance_array_keys(IndexScanDesc scan, BTReadPageState *pstate,
 	 * for _bt_check_compare to behave as if they are required in the current
 	 * scan direction to deal with NULLs.  We'll account for that separately.)
 	 */
-	Assert((_bt_tuple_before_array_skeys(scan, dir, tuple, tupdesc, tupnatts,
-										 false, 0, NULL) ==
-			!all_required_satisfied) || pstate->beyondskip);
+	if (!(_bt_tuple_before_array_skeys(scan, dir, tuple, tupdesc, tupnatts,
+									   false, 0,
+									   NULL) == !all_required_satisfied ||
+		  pstate->beyondskip))
+	{
+		char	   *pitup;
+
+		pitup = _nbtree_print_itup(tuple, rel);
+
+		elog(ERROR, "later pitup: %s\n\n %s", pitup, so->debugstr.data);
+
+		if (pitup)
+			pfree(pitup);
+	}
 
 	/*
 	 * Optimization: if a scan with a skip array required "beyond end of array
@@ -5185,8 +5207,19 @@ _bt_checkkeys(IndexScanDesc scan, BTReadPageState *pstate, bool arrayKeys,
 		Assert(!so->scanBehind && !so->oppositeDirCheck);
 		Assert(!pstate->beyondskip && !pstate->prechecked &&
 			   !pstate->firstmatch);
-		Assert(!_bt_tuple_before_array_skeys(scan, dir, tuple, tupdesc,
-											 tupnatts, false, 0, NULL));
+		if (!(!_bt_tuple_before_array_skeys(scan, dir, tuple, tupdesc,
+											tupnatts, false, 0, NULL)))
+		{
+			char	   *pitup;
+
+			pitup = _nbtree_print_itup(tuple, scan->indexRelation);
+
+			elog(ERROR, "_bt_checkkeys precheck call: %s\n\n %s", pitup, so->debugstr.data);
+
+			if (pitup)
+				pfree(pitup);
+
+		}
 	}
 	if (pstate->prechecked || pstate->firstmatch)
 	{
