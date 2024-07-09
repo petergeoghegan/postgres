72805613cec76ef1fdebcbc4d632fd1e7f08787c
Peter Geoghegan <pg@bowt.ie>
2024-07-09 14:56:03 -0400

    DEBUG

--- src/backend/access/nbtree/nbtutils.c
+++ src/backend/access/nbtree/nbtutils.c
@@ -3123,8 +3123,19 @@ _bt_advance_array_keys(IndexScanDesc scan, BTReadPageState *pstate,
 		/*
 		 * Precondition array state assertion
 		 */
-		Assert(!_bt_tuple_before_array_skeys(scan, dir, tuple, tupdesc,
-											 tupnatts, false, 0, NULL));
+		if (!(!_bt_tuple_before_array_skeys(scan, dir, tuple, tupdesc,
+											tupnatts, false, 0, NULL)))
+		{
+			char	   *pitup;
+
+			pitup = _nbtree_print_itup(tuple, rel);
+
+			elog(ERROR, "pitup: %s\n\n %s", pitup, so->debugstr.data);
+
+			if (pitup)
+				pfree(pitup);
+
+		}
 
 		so->scanBehind = false; /* reset */
 
@@ -3646,9 +3657,19 @@ _bt_advance_array_keys(IndexScanDesc scan, BTReadPageState *pstate,
 	 * for _bt_check_compare to behave as if they are required in the current
 	 * scan direction to deal with NULLs.  We'll account for that separately.)
 	 */
-	Assert(_bt_tuple_before_array_skeys(scan, dir, tuple, tupdesc, tupnatts,
-										false, 0, NULL) ==
-		   !all_required_satisfied);
+	if (!(_bt_tuple_before_array_skeys(scan, dir, tuple, tupdesc, tupnatts,
+									   false, 0,
+									   NULL) == !all_required_satisfied))
+	{
+		char	   *pitup;
+
+		pitup = _nbtree_print_itup(tuple, rel);
+
+		elog(ERROR, "later pitup: %s\n\n %s", pitup, so->debugstr.data);
+
+		if (pitup)
+			pfree(pitup);
+	}
 
 	/*
 	 * We generally permit primitive index scans to continue onto the next
