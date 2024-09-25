8cea91e5db60871d2bdff6b679e1d149bc23567f
Peter Geoghegan <pg@bowt.ie>
2024-09-25 13:55:44 -0400

    Revert "DEBUG: make _bt_advance_array_keys invariant violations into errors"
    This reverts commit aa8c493adcaa791454656a53a547cf971a5b08df.

--- src/backend/access/nbtree/nbtree.c
+++ src/backend/access/nbtree/nbtree.c
@@ -253,14 +253,6 @@ btgettuple(IndexScanDesc scan, ScanDirection dir)
 	/* btree indexes are never lossy */
 	scan->xs_recheck = false;
 
-	/* reset bmsPages so that we don't give false positives with cursors */
-	if (so->bmsPagesDir != dir)
-	{
-		bms_free(so->bmsPages);
-		so->bmsPages = NULL;
-		so->bmsPagesDir = dir;
-	}
-
 	/* Each loop iteration performs another primitive index scan */
 	do
 	{
@@ -396,8 +388,6 @@ btbeginscan(Relation rel, int nkeys, int norderbys)
 	else
 		so->keyData = NULL;
 
-	so->bmsPages = NULL;
-	so->bmsPagesDir = NoMovementScanDirection;
 	so->needPrimScan = false;
 	so->scanBehind = false;
 	so->oppositeDirCheck = false;
@@ -504,10 +494,6 @@ btrescan(IndexScanDesc scan, ScanKey scankey, int nscankeys,
 		memcpy(scan->keyData, scankey, scan->numberOfKeys * sizeof(ScanKeyData));
 	so->numberOfKeys = 0;		/* until _bt_preprocess_keys sets it */
 	so->numArrayKeys = 0;		/* ditto */
-
-	bms_free(so->bmsPages);
-	so->bmsPages = NULL;
-	so->bmsPagesDir = NoMovementScanDirection;
 }
 
 /*
@@ -646,9 +632,6 @@ btrestrpos(IndexScanDesc scan)
 			{
 				_bt_start_array_keys(scan, so->currPos.dir);
 				so->needPrimScan = false;
-				bms_free(so->bmsPages);
-				so->bmsPages = NULL;
-				so->bmsPagesDir = NoMovementScanDirection;
 			}
 		}
 		else
--- src/backend/access/nbtree/nbtsearch.c
+++ src/backend/access/nbtree/nbtsearch.c
@@ -1211,8 +1211,6 @@ _bt_first(IndexScanDesc scan, ScanDirection dir)
 
 	Assert(!BTScanPosIsValid(so->currPos));
 
-	CHECK_FOR_INTERRUPTS();
-
 	/*
 	 * Examine the scan keys and eliminate any redundant keys; also mark the
 	 * keys that must be matched to continue the scan.
@@ -2215,17 +2213,6 @@ _bt_readpage(IndexScanDesc scan, ScanDirection dir, OffsetNumber offnum,
 	 * good.
 	 */
 	Assert(BTScanPosIsPinned(so->currPos));
-
-	if (so->arrayKeys)
-	{
-		int			pageNum = (int) BufferGetBlockNumber(so->currPos.buf);
-
-		if ((ScanDirectionIsForward(dir) || offnum >= minoff) &&
-			bms_is_member(pageNum, so->bmsPages))
-			elog(ERROR, "pageNum %d already visited\n\n%s", pageNum,
-				 so->debugstr.data);
-		so->bmsPages = bms_add_member(so->bmsPages, pageNum);
-	}
 
 	/*
 	 * Prechecking the value of the continuescan flag for the last item on the
