c014bb4211e63d6d0791ca157a3efa2b117da037
Tomas Vondra <tomas@vondra.me>
2024-09-30 22:48:12 +0200

    WIP: index prefetching
    Allows the index AM to provide items (TIDs and tuples) in batches, which
    is then used to implement prefetching of heap tuples in index scans
    (including index-only scans). This is similar to prefetching already
    done in bitmap scans, and can result in significant speedups.
    
    The index AM may implement an optional "amgetbatch" callback, returning
    a batch of items. The indexam.c code then handles this transparently
    through the existing "getnext" interface.
    
    It is up to the index AM to return only batches that it can handle
    internally. For example, most of the later patches adding support for
    batching to relevant index AMs (btree, hash, gist, sp-gist) restrict the
    batches to a single leaf page. This makes implementation of batching
    much simpler, with only minimal changes to the index AMs, but it's not a
    hard requirement. The index AM can produce batches spanning arbitrary
    number of leaf pages. This is left as a possible future improvement.
    
    Most of the batching/prefetching logic happens in indexam.c. This means
    the executor code can continue to call the interface just like before.
    
    The only "violation" happens in index-only scans, which need to check
    the visibility map both when the prefetching pages (we don't want to
    prefetch pages that are unnecessary) and later when reading the data.
    For cached data the visibility map checks can be fairly expensive, so
    it's desirable to keep and reuse the result of the first check.
    
    At the moment, the prefetching does not handle mark/restore plans. This
    is doable, but requires additional synchronization between the batching
    and index AM code in the "opposite direction".
    
    This patch does not actually add batching to any of the index AMs, it's
    just the common infrastructure.
    
    TODO Add the new index AM callback to sgml docs.
    
    Re-introduce the callback to check VM and remember the result.
    
    It can happen the first few batches (leaf pages) may be returned from
    the index, skipping the heap fetches. Which means the read stream does
    no reads until much later after the first batches are already freed.
    Because the reads only happen when first reading from the stream. In
    that case we need to be careful about initializing the stream position
    because setting it to (0,0) would be wrong as the batch is already gone.
    So just initialize to readPost, which should be initialized already.
    
    Could it happen later, or just on first call? Probably first call only,
    as the read stream always looks ahead for the block that actually needs
    reading.

--- src/include/access/genam.h
+++ src/include/access/genam.h
@@ -15,6 +15,7 @@
 #define GENAM_H
 
 #include "access/htup.h"
+#include "access/itup.h"
 #include "access/sdir.h"
 #include "access/skey.h"
 #include "nodes/tidbitmap.h"
@@ -111,6 +112,7 @@ typedef bool (*IndexBulkDeleteCallback) (ItemPointer itemptr, void *state);
 
 /* struct definitions appear in relscan.h */
 typedef struct IndexScanDescData *IndexScanDesc;
+typedef struct IndexScanBatchData *IndexScanBatch;
 typedef struct SysScanDescData *SysScanDesc;
 
 typedef struct ParallelIndexScanDescData *ParallelIndexScanDesc;
@@ -154,6 +156,8 @@ typedef struct IndexOrderByDistance
 /*
  * generalized index_ interface routines (in indexam.c)
  */
+
+extern PGDLLIMPORT bool enable_indexscan_batching;
 
 /*
  * IndexScanIsValid
@@ -213,7 +217,6 @@ extern bool index_fetch_heap(IndexScanDesc scan, struct TupleTableSlot *slot);
 extern bool index_getnext_slot(IndexScanDesc scan, ScanDirection direction,
 							   struct TupleTableSlot *slot);
 extern int64 index_getbitmap(IndexScanDesc scan, TIDBitmap *bitmap);
-
 extern IndexBulkDeleteResult *index_bulk_delete(IndexVacuumInfo *info,
 												IndexBulkDeleteResult *istat,
 												IndexBulkDeleteCallback callback,
@@ -231,7 +234,7 @@ extern void index_store_float8_orderby_distances(IndexScanDesc scan,
 												 bool recheckOrderBy);
 extern bytea *index_opclass_options(Relation indrel, AttrNumber attnum,
 									Datum attoptions, bool validate);
-
+extern IndexScanBatch index_batch_alloc(int maxitems, bool want_itup);
 
 /*
  * index access method support routines (in genam.c)
