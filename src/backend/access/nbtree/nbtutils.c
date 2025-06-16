efe77401fd1d069f2be534d3e2303e73476555de
Peter Geoghegan <pg@bowt.ie>
2025-06-16 16:12:50 -0400

    DEBUG Add instrumentation.

--- src/backend/access/nbtree/nbtutils.c
+++ src/backend/access/nbtree/nbtutils.c
@@ -3529,7 +3529,12 @@ _bt_killitems(IndexScanDesc scan)
 				 * (there might be a later tuple whose TID is a match).
 				 */
 				if (j == nposting)
+				{
 					killtuple = true;
+					elog(WARNING, "\"%s\" block %u killing posting list with %d items at page offset number %u",
+						 RelationGetRelationName(rel), so->currPos.currPage,
+						 nposting, offnum);
+				}
 			}
 			else if (ItemPointerEquals(&ituple->t_tid, &kitem->heapTid))
 				killtuple = true;
