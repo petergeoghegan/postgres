4a58fdc71b35e303acbf2f61b496bf16a0d4c4e7
Tomas Vondra <tomas@vondra.me>
2025-08-19 14:45:32 +0200

    explain: show prefetch stats for indexes
    Shows various prefetch-related stats in EXPLAIN. This includes average
    prefetch distance, number of prefetches, stalls, skipped blocks etc.
    
    The stats are tracked by the read_stream, and get exposed to explain by
    calling index_get_prefetch_stats. This copies the counters from the
    read_stream into instrumentation.
    
    Notes:
    
    - One annoying thing is that calling index_get_prefetch_stats from
      ExecEndIndexScan does not work, because EXPLAIN gets called before
      that. It's enough for workers, though. So it's called from the
      explain code, which does the trick for the leader.
    
    - Maybe we could have a stats struct in the index scan state, and only
      pass it to the read_stream? Then the stream would update it directly.
    
    - We're still passing individual counters around. Maybe we should have
      a struct, and pass that around.

--- src/backend/access/heap/heapam_handler.c
+++ src/backend/access/heap/heapam_handler.c
@@ -831,7 +831,10 @@ heapam_getnext_stream(ReadStream *stream, void *callback_private_data,
 
 			/* item is known to be all-visible; prefetching isn't required */
 			if (item->allVisible)
+			{
+				read_stream_skip_block(stream);
 				continue;
+			}
 		}
 
 		if (prefetchBlock == batchringbuf->currentPrefetchBlock)
@@ -841,6 +844,7 @@ heapam_getnext_stream(ReadStream *stream, void *callback_private_data,
 			 * block number; we must not return the same prefetchBlock twice
 			 * (twice in succession)
 			 */
+			read_stream_skip_block(stream);
 			continue;
 		}
 
