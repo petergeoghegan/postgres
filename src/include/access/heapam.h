b843d2aa90b823ad6418b223ef8710d5c0bc088b
Peter Geoghegan <pg@bowt.ie>
2025-09-09 19:50:03 -0400

    Add batching interfaces used by heapam and nbtree.
    Add a new amgetbatch index AM interface that allows index access methods
    to implement plain/ordered index scans that return index entries in
    per-leaf-page batches, rather than one at a time.  This enables a
    variety of optimizations on the table AM side, most notably I/O
    prefetching of heap tuples during ordered index scans.  It will also
    enable an optimization that has heapam avoid repeatedly locking and
    unlocking the same heap page's buffer.
    
    Index access methods that support plain index scans must now implement
    either the amgetbatch interface OR the amgettuple interface.  The
    amgettuple interface will still be used by index AMs that require direct
    control over the progress of index scans (e.g., GiST with KNN ordered
    scans).
    
    This commit also adds a new table AM interface callback, called by the
    core executor through the new table_index_getnext_slot shim function.
    This allows the table AM to directly manage the progress of index scans
    rather than having individual TIDs passed in by the caller. The
    amgetbatch interface is tightly coupled with the new approach to ordered
    index scans added to the table AM.  The table AM can apply knowledge of
    which TIDs will be returned to scan in the near future to optimize and
    batch table AM block accesses, and to perform I/O prefetching.  These
    optimizations are left as work for later commits.
    
    Batches returned from amgetbatch are guaranteed to be associated with an
    index page containing at least one matching tuple.  The amgetbatch
    interface may hold buffer pins as interlocks against concurrent TID
    recycling by VACUUM.  This extends/generalizes the mechanism added to
    nbtree by commit 2ed5b87f to all index AMs that add support for the new
    amgetbatch interface.
    
    Author: Tomas Vondra <tomas@vondra.me>
    Author: Peter Geoghegan <pg@bowt.ie>
    Reviewed-By: Andres Freund <andres@anarazel.de>
    Reviewed-By: Thomas Munro <thomas.munro@gmail.com>
    Discussion: https://postgr.es/m/cf85f46f-b02f-05b2-5248-5000b894ebab@enterprisedb.com
    Discussion: https://postgr.es/m/efac3238-6f34-41ea-a393-26cc0441b506%40vondra.me
    Discussion: https://postgr.es/m/CAH2-Wzk9%3Dx%3Da2TbcqYcX%2BXXmDHQr5%3D1v9m4Z_v8a-KwF1Zoz0A%40mail.gmail.com

--- src/include/access/heapam.h
+++ src/include/access/heapam.h
@@ -117,7 +117,13 @@ typedef struct IndexFetchHeapData
 	IndexFetchTableData xs_base;	/* AM independent part of the descriptor */
 
 	Buffer		xs_cbuf;		/* current heap buffer in scan, if any */
+	BlockNumber xs_blk;			/* xs_cbuf's block number, if any */
 	/* NB: if xs_cbuf is not InvalidBuffer, we hold a pin on that buffer */
+
+	Buffer		vmbuf;			/* visibility map buffer */
+	TupleTableSlot *ios_tableslot;	/* transient slot for fetching tuples to
+									 * check visibility during index-only
+									 * scans */
 } IndexFetchHeapData;
 
 /* Result codes for HeapTupleSatisfiesVacuum */
