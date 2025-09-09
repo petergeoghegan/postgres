8f140040aea734a4b1d0e5acd069e9ae9bbf6471
Peter Geoghegan <pg@bowt.ie>
2025-09-09 19:50:03 -0400

    Add interfaces that enable index prefetching.
    Add a new amgetbatch index AM interface that allows index access methods
    to implement plain/ordered index scans that return index entries in
    batches comprising all matching items from an index page, rather than
    one match at a time.
    
    This commit also adds a new table AM interface callback, called by the
    core executor through the new table_index_getnext_slot shim function.
    This allows the table AM to directly manage the progress of index scans
    rather than having individual TIDs passed in by the caller one by one.
    The amgetbatch interface is tightly coupled with the new approach to
    index scans added to the table AM.  The table AM can apply knowledge of
    which TIDs will be returned to the scan in the near future to perform
    I/O prefetching.  Prefetching will be added by an upcoming commit.
    
    Index access methods that support plain index scans must now implement
    either the amgetbatch interface OR the amgettuple interface.  The
    amgettuple interface will still be used by index AMs that require direct
    control over the progress of index scans (e.g., GiST with KNN ordered
    scans).  Almost all existing callers that perform index scans now use
    the new table_index_getnext_slot interface, regardless of whether the
    underlying index AM uses amgetbatch or amgettuple.
    
    The amgetbatch interface returns batches that hold a buffer pin on an
    index page that can be used by the table AM as an interlock against
    concurrent TID recycling by VACUUM.  Now heapam only needs to hold on to
    such a pin for an instant -- except during scans that use a non-MVCC
    snapshot.  Non-MVCC scans continue to need to hold the pin until all of
    the batch's TIDs have been fetched from the heap.
    
    This extends the dropPin mechanism added to nbtree by commit 2ed5b87f,
    and generalizes it to work with all index AMs that support the new
    amgetbatch interface.  We can always safely drop index page pins
    eagerly, provided the scan uses an MVCC snapshot (unlike the nbtree
    dropPin optimization, which had a couple of additional restrictions).
    
    An upcoming commit that will add index prefetching will use a read
    stream to read heap pages during index scans.  Read stream is careful to
    limit how many things it pins, lest we run into problems due to having
    too many buffers pinned.  Simply never holding on to index page buffer
    pins greatly simplifies resource management for index prefetching;
    there's no risk of unintended interactions between the read stream and
    index AM.  The only downside is that we cannot support prefetching
    during scans that use a non-MVCC snapshot, which seems quite acceptable.
    
    In practice, heapam doesn't drop each batch's index page buffer pin at
    the earliest opportunity during index-only scans.  This was deemed
    necessary to avoid regressing index-only scans with a LIMIT, in
    particular with nestloop anti-joins and nestloop semi-joins; eagerly
    loading all the visibility information up front regressed such queries.
    The new amgetbatch interface gives table AMs the authority to decide
    when and where to drop index page pins, so this can be considered a
    heapam implementation detail (index AMs don't need to know about it).
    This scheme still allows index prefetching to consistently hold no more
    than one batch index page pin at a time, even when an index-only scan
    (that must perform some heap fetches) holds open several index batches
    at once in order to maintain an adequate prefetch distance.
    
    Author: Tomas Vondra <tomas@vondra.me>
    Author: Peter Geoghegan <pg@bowt.ie>
    Reviewed-By: Andres Freund <andres@anarazel.de>
    Reviewed-By: Thomas Munro <thomas.munro@gmail.com>
    Discussion: https://postgr.es/m/cf85f46f-b02f-05b2-5248-5000b894ebab@enterprisedb.com
    Discussion: https://postgr.es/m/efac3238-6f34-41ea-a393-26cc0441b506%40vondra.me

--- src/include/access/heapam.h
+++ src/include/access/heapam.h
@@ -117,8 +117,34 @@ typedef struct IndexFetchHeapData
 	IndexFetchTableData xs_base;	/* AM independent part of the descriptor */
 
 	Buffer		xs_cbuf;		/* current heap buffer in scan, if any */
-	/* NB: if xs_cbuf is not InvalidBuffer, we hold a pin on that buffer */
+	BlockNumber xs_blk;			/* xs_cbuf's block number, if any */
+
+	/* For index-only scans that must access the visibility map */
+	Buffer		xs_vmbuf;		/* visibility map buffer */
+	int			xs_vm_items;	/* # items to resolve visibility info for */
+
+	bool		xs_lastinblock; /* last TID on this block in current batch? */
+
+	/* NB: if xs_cbuf or vmbuf are not InvalidBuffer, we hold a pin */
 } IndexFetchHeapData;
+
+/*
+ * Per-batch data private to the heap table AM.
+ *
+ * Stored at a negative offset from the IndexScanBatch pointer, in the
+ * table AM opaque area of each batch allocation.
+ */
+typedef struct HeapBatchData
+{
+	uint8	   *visInfo;		/* per-item visibility flags, or NULL */
+} HeapBatchData;
+
+/* Access the heap-private per-batch data from an IndexScanBatch pointer */
+static inline HeapBatchData *
+heap_batch_data(IndexScanBatch batch, IndexScanDesc scan)
+{
+	return (HeapBatchData *) ((char *) batch - scan->batch_table_offset);
+}
 
 /* Result codes for HeapTupleSatisfiesVacuum */
 typedef enum
