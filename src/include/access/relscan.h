4f2f7dad5ad9b914a2fbc3d66dc6f72d2542ff6d
Peter Geoghegan <pg@bowt.ie>
2025-11-15 14:03:58 -0500

    Add I/O prefetching to amgetbatch index scans.
    Perform I/O prefetching during plain index scans, and index-only
    scans for index AMs that use the recently added amgetbatch interface.
    
    Tentative plan is to move all prefetching inside a new batch-orientated
    table AM interface that puts the table AM in direct control of ordered
    index scans.  That'll obviate the need for index-only scans to store
    authoritative visibility information so as to avoid readPos and
    streamPos having inconsistent ideas about which heap blocks are
    all-visible.  We leave those parts in for now.

--- src/include/access/relscan.h
+++ src/include/access/relscan.h
@@ -20,6 +20,7 @@
 #include "nodes/tidbitmap.h"
 #include "port/atomics.h"
 #include "storage/buf.h"
+#include "storage/read_stream.h"
 #include "storage/relfilelocator.h"
 #include "storage/spin.h"
 #include "utils/relcache.h"
@@ -122,6 +123,7 @@ typedef struct ParallelBlockTableScanWorkerData *ParallelBlockTableScanWorker;
 typedef struct IndexFetchTableData
 {
 	Relation	rel;
+	ReadStream *rs;
 } IndexFetchTableData;
 
 /*
@@ -201,12 +203,17 @@ typedef struct BatchIndexScanData
 	 * is of size BLCKSZ, so it can hold as much as a full page's worth of
 	 * tuples.
 	 */
+	char	   *itemsvisibility;	/* Index-only scan visibility cache */
 	char	   *currTuples;		/* tuple storage for items[] */
 	int			maxitems;		/* allocated size of items[] */
 	BatchMatchingItem items[FLEXIBLE_ARRAY_MEMBER];
 } BatchIndexScanData;
 
 typedef struct BatchIndexScanData *BatchIndexScan;
+typedef struct IndexScanDescData IndexScanDescData;
+typedef bool (*IndexPrefetchCallback) (IndexScanDescData * scan,
+									   void *arg,
+									   BatchQueueItemPos *pos);
 
 /*
  * State used by amgetbatch index AMs to manage a queue of batches of items
@@ -226,6 +233,27 @@ typedef struct BatchQueue
 	 * it's over.
 	 */
 	bool		finished;
+	bool		reset;
+
+	/*
+	 * Did we disable prefetching/use of a read stream because it didn't pay
+	 * for itself?
+	 */
+	bool		prefetchingLockedIn;
+	bool		disabled;
+
+	/*
+	 * During prefetching, currentPrefetchBlock is the table AM block number
+	 * that was returned by our read stream callback most recently.  Used to
+	 * suppress duplicate successive read stream block requests.
+	 *
+	 * Prefetching can still perform non-successive requests for the same
+	 * block number (in general we're prefetching in exactly the same order
+	 * that the scan will return table AM TIDs in).  We need to avoid
+	 * duplicate successive requests because table AMs expect to be able to
+	 * hang on to buffer pins across table_index_fetch_tuple calls.
+	 */
+	BlockNumber currentPrefetchBlock;
 
 	/*
 	 * Current scan direction, for the currently loaded batches. This is used
@@ -236,6 +264,7 @@ typedef struct BatchQueue
 	/* current positions in batches[] for scan */
 	BatchQueueItemPos readPos;	/* read position */
 	BatchQueueItemPos markPos;	/* mark/restore position */
+	BatchQueueItemPos streamPos;	/* stream position (for prefetching) */
 
 	BatchIndexScan markBatch;
 
@@ -257,6 +286,10 @@ typedef struct BatchQueue
 	}			cache;
 
 	BatchIndexScan *batches;
+
+	/* callback to skip prefetching in IOS etc. */
+	IndexPrefetchCallback prefetch;
+	void	   *prefetchArg;
 
 } BatchQueue;
 
