2ca7c633cb4252e78400e119b1cc5c9ecefebf29
Peter Geoghegan <pg@bowt.ie>
2025-11-26 16:55:55 -0500

    Revert "REVERT ME disable less interesting test"
    This reverts commit d90868d20021ad9c86292a8571fef0285dac8e98.

--- src/test/regress/sql/small_dynamic_saop_advancement.sql
+++ src/test/regress/sql/small_dynamic_saop_advancement.sql
@@ -2,45 +2,45 @@
 -- HASH_INDEX
 --
 
--- -- directory paths are passed to us in environment variables
--- set client_min_messages=error;
--- drop table if exists hash_i4_heap;
--- reset client_min_messages;
+-- directory paths are passed to us in environment variables
+set client_min_messages=error;
+drop table if exists hash_i4_heap;
+reset client_min_messages;
+
+CREATE TABLE hash_i4_heap (
+	seqno 		int4,
+	random 		int4
+);
+
+\set filename '/mnt/nvme/postgresql/patch/source/src/test/regress/data/hash.data'
+COPY hash_i4_heap FROM :'filename';
+
+-- the data in this file has a lot of duplicates in the index key
+-- fields, leading to long bucket chains and lots of table expansion.
+-- this is therefore a stress test of the bucket overflow code (unlike
+-- the data in hash.data, which has unique index keys).
 --
--- CREATE TABLE hash_i4_heap (
--- 	seqno 		int4,
--- 	random 		int4
--- );
+-- \set filename :abs_srcdir '/data/hashovfl.data'
+-- COPY hash_ovfl_heap FROM :'filename';
+
+ANALYZE hash_i4_heap;
+
+CREATE INDEX hash_i4_index ON hash_i4_heap USING hash (random int4_ops);
+
+SELECT * FROM hash_i4_heap
+   WHERE hash_i4_heap.random = 843938989;
+EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF)
+SELECT * FROM hash_i4_heap
+   WHERE hash_i4_heap.random = 843938989;
+
 --
--- \set filename '/mnt/nvme/postgresql/patch/source/src/test/regress/data/hash.data'
--- COPY hash_i4_heap FROM :'filename';
+-- leak
 --
--- -- the data in this file has a lot of duplicates in the index key
--- -- fields, leading to long bucket chains and lots of table expansion.
--- -- this is therefore a stress test of the bucket overflow code (unlike
--- -- the data in hash.data, which has unique index keys).
--- --
--- -- \set filename :abs_srcdir '/data/hashovfl.data'
--- -- COPY hash_ovfl_heap FROM :'filename';
---
--- ANALYZE hash_i4_heap;
---
--- CREATE INDEX hash_i4_index ON hash_i4_heap USING hash (random int4_ops);
---
--- SELECT * FROM hash_i4_heap
---    WHERE hash_i4_heap.random = 843938989;
--- EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF)
--- SELECT * FROM hash_i4_heap
---    WHERE hash_i4_heap.random = 843938989;
---
--- --
--- -- leak
--- --
--- SELECT * FROM hash_i4_heap
---    WHERE hash_i4_heap.random = 66766766;
--- EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF)
--- SELECT * FROM hash_i4_heap
---    WHERE hash_i4_heap.random = 66766766;
+SELECT * FROM hash_i4_heap
+   WHERE hash_i4_heap.random = 66766766;
+EXPLAIN (ANALYZE, BUFFERS, TIMING OFF, SUMMARY OFF)
+SELECT * FROM hash_i4_heap
+   WHERE hash_i4_heap.random = 66766766;
 
 --
 -- doublefree.
