3f79974f9d4b6b944d6c8a7cce9f3a16304ca1fa
Peter Geoghegan <pg@bowt.ie>
2026-01-27 11:24:21 -0500

    Revert "[skip ci] Experiment: don't prefetch when batch we're moving from has a mark"
    This reverts commit 26ab1787ba6916cc7a4cb1c28a41bdc73ccc51f7.

--- src/backend/access/heap/heapam_handler.c
+++ src/backend/access/heap/heapam_handler.c
@@ -507,7 +507,6 @@ heapam_batch_getnext(IndexScanDesc scan, ScanDirection direction,
 		 */
 		if (!scan->xs_heapfetch->rs && priorBatch && scan->MVCCScan &&
 			(scan->tuples_needed == -1 || scan->tuples_needed > 10) &&
-			priorBatch != batchringbuf->markBatch &&
 			enable_indexscan_prefetch)
 		{
 			Assert(!batchringbuf->prefetchPos.valid);
