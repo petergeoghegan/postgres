d3800c5a5b7e0bf623082851c4bf1fc404620349
Peter Geoghegan <pg@bowt.ie>
2026-01-27 21:20:50 -0500

    DEBUG: Add BATCH_CACHE_DEBUG

--- src/include/access/relscan.h
+++ src/include/access/relscan.h
@@ -245,6 +245,8 @@ typedef struct IndexScanBatchData *IndexScanBatch;
 #define INDEX_SCAN_MAX_BATCHES		64
 #define INDEX_SCAN_CACHE_BATCHES	2
 
+/* #define BATCH_CACHE_DEBUG */
+
 /*
  * State used by table AMs to manage an index scan that uses the amgetbatch
  * interface.  Scans use a ring buffer of batches returned by amgetbatch.
@@ -322,6 +324,16 @@ typedef struct BatchRingBuffer
 
 	/* number of items to resolve during visibility checks */
 	int			vmItems;
+
+#ifdef BATCH_CACHE_DEBUG
+	/* Batch cache efficiency stats (for debugging/analysis) */
+	uint64		cacheHits;		/* batches reused from cache */
+	uint64		cacheMisses;	/* batches newly allocated */
+	uint64		batchesReturned;	/* total batches returned by amgetbatch */
+	uint64		batchesScanned; /* batches consumed by scanPos */
+	uint64		rescans;		/* number of index_rescan calls */
+	uint8		batchHighWatermark; /* max batches in ring buffer at once */
+#endif
 
 } BatchRingBuffer;
 
@@ -479,6 +491,16 @@ index_scan_batch_append(IndexScanDescData *scan, IndexScanBatch batch)
 
 	ringbuf->batches[nextBatch & (INDEX_SCAN_MAX_BATCHES - 1)] = batch;
 	ringbuf->nextBatch++;
+
+#ifdef BATCH_CACHE_DEBUG
+	ringbuf->batchesReturned++;
+	{
+		uint8		count = index_scan_batch_count(scan);
+
+		if (count > ringbuf->batchHighWatermark)
+			ringbuf->batchHighWatermark = count;
+	}
+#endif
 }
 
 /*
