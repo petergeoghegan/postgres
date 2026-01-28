3364978439a7f5a41ff9363b560e74c458233e41
Peter Geoghegan <pg@bowt.ie>
2026-01-28 16:45:17 -0500

    Revert "explain: show prefetch stats for indexes"
    This reverts commit e0f390575321e85238d75ca6c9aeb4822491f5ee.

--- src/backend/access/heap/heapam_handler.c
+++ src/backend/access/heap/heapam_handler.c
@@ -879,10 +879,7 @@ heapam_getnext_stream(ReadStream *stream, void *callback_private_data,
 
 			/* item is known to be all-visible; prefetching isn't required */
 			if (item->allVisible)
-			{
-				read_stream_skip_block(stream);
 				continue;
-			}
 		}
 
 		if (prefetchBlock == hscan->xs_prefetch_block)
@@ -892,7 +889,6 @@ heapam_getnext_stream(ReadStream *stream, void *callback_private_data,
 			 * block number; we must not return the same prefetchBlock twice
 			 * (twice in succession)
 			 */
-			read_stream_skip_block(stream);
 			continue;
 		}
 
