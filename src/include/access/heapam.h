10d1811e0797aa75c76fd20466d9974735c540b5
Peter Geoghegan <pg@bowt.ie>
2026-03-22 02:22:06 -0400

    heapam: Optimize pin transfers during index scans.
    Add an xs_lastinblock flag to IndexFetchHeapData that tracks whether the
    current TID is the last one on its heap block within the current batch.
    When it is, heapam_index_fetch_tuple can transfer its buffer pin to the
    slot (via ExecStorePinnedBufferHeapTuple) instead of incrementing the
    pin count, saving a pair of IncrBufferRefCount/ReleaseBuffer calls.
    This optimization is not used for index-only scans because all-visible
    items can be skipped, which would break block deduplication symmetry
    between the scan and the read stream (besides, the performance of this
    code path can only matter when many heap fetches are required, which is
    hopefully rare during index-only scans).
    
    Also add an explicit ExecClearTuple to the block-switch path in
    heapam_index_fetch_tuple_impl to release the pin transferred to the slot
    on the previous call (just before calling ReleaseBuffer as part of
    moving on to the scan's next block).  This fixes a performance problem
    where the code path in question triggers GetPrivateRefCountEntrySlow
    calls more often than one would hope.  The underlying issue has been
    tied to the pin in the slot being held, even if we decide to release the
    buffer and move on: ExecStoreBufferHeapTuple will first fail to hit the
    backend-local cache for the release of the old pin (because we just
    pinned and locked the new buffer), causing a cache miss inside
    IncrBufferRefCount.
    
    Author: Peter Geoghegan <pg@bowt.ie>
    Suggested-by: Andres Freund <andres@anarazel.de>
    Reviewed-By: Andres Freund <andres@anarazel.de>
    Discussion: https://postgr.es/m/CAH2-Wz=D4Lru9BkvqaRnFRPDaZbfTOdWcxw13zyG6GVFTtz_vw@mail.gmail.com

--- src/include/access/heapam.h
+++ src/include/access/heapam.h
@@ -131,6 +131,9 @@ typedef struct IndexFetchHeapData
 	int			xs_vm_items;	/* # items to resolve visibility info for */
 
 	bool		xs_readonly;	/* scan is read-only? */
+
+	/* Plain index scan xs_lastinblock optimization */
+	bool		xs_lastinblock; /* last TID on this block in current batch? */
 } IndexFetchHeapData;
 
 /* Result codes for HeapTupleSatisfiesVacuum */
