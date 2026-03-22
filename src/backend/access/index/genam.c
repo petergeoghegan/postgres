5e16b23f4d7b6cd2c238d8b2f1f11b8f26c5434f
Peter Geoghegan <pg@bowt.ie>
2026-03-22 02:36:57 -0400

    Add slot-based table AM index scan interface.
    Add table_index_getnext_slot, a new table AM callback that wraps both
    plain and index-only index scans that use amgettuple.  Two new
    TableAmRoutine callbacks are introduced -- one for plain scans and one
    for index-only scans -- which an upcoming commit that adds the
    amgetbatch interface will expand to four.  The appropriate callback is
    resolved once in index_beginscan, and called through a function pointer
    (xs_getnext_slot) on the IndexScanDesc when the table_index_getnext_slot
    shim function is called from executor nodes.
    
    This moves VM checks for index-only scans out of the executor and into
    heapam, enabling batching of visibility map lookups (though for now we
    continue to just perform retail lookups).  Using the new higher level
    slot-based interface greatly simplifies nodeIndexonlyscan.c, which no
    longer has to deal with the visibility map directly.  More importantly,
    this is a significant architectural improvement: table AMs can now
    implement index-only scans that are not tied to heapam's visibility map.
    
    A small minority of callers (2 callers in total) fundamentally need to
    pass a TID to the table AM (both perform constraint enforcement).  These
    callers don't actually perform index scans (even if their TIDs are taken
    from an index), and have no need for most of the index scan machinery.
    Switch these callers over to the new fetch_tid interface (which replaces
    the previous TID-based index_fetch_tuple interface).  All index scan
    callers now use the new slot-based interface (table_index_getnext_slot).
    
    Index-only scan callers pass table_index_getnext_slot a TupleTableSlot
    (which the table AM uses internally for heap fetches), but continue to
    read their results from IndexScanDescData fields such as xs_itup (rather
    than from the slot itself).  All callers can continue to rely on the
    scan descriptor's xs_heaptid field being set on each call.
    
    The VISITED_PAGES_LIMIT mechanism used by get_actual_variable_range to
    cap scan overhead during planning is reworked to go through a new scan
    descriptor interface (xs_visited_pages_limit), rather than tracking the
    costs directly and terminating the scan itself, in an ad-hoc way.  This
    is necessary because callers that use the new slot-based interface no
    longer have direct access to which heap blocks were fetched.  Similarly,
    nodeIndexonlyscan.c can no longer use InstrCountTuples2 to count heap
    fetches during an EXPLAIN ANALYZE.  EXPLAIN ANALYZE now obtains this
    information from a new IndexScanInstrumentation field, which table AMs
    are required to maintain.
    
    Though independently useful, this commit is preparatory work for an
    upcoming commit that will add an amgetbatch index AM interface, where
    the table AM takes full responsibility for managing the progress of
    index scans.  That will move most of the implementation of scrollable
    cursors out of index AMs and into table AMs, making it essential that
    executor nodes pass the current scan direction down to the table AM.
    
    The heapam implementations make aggressive use of forced inlining to
    ensure that plain and index-only code paths are fully specialized at
    compile time despite sharing a common implementation.  Testing has shown
    this is necessary to keep icache misses to a minimum, at least with the
    two upcoming amgetbatch variants.
    
    Author: Peter Geoghegan <pg@bowt.ie>
    Reviewed-By: Andres Freund <andres@anarazel.de>
    Reviewed-By: Tomas Vondra <tomas@vondra.me>
    Discussion: https://postgr.es/m/CAH2-WzmYqhacBH161peAWb5eF=Ja7CFAQ+0jSEMq=qnfLVTOOg@mail.gmail.com

--- src/backend/access/index/genam.c
+++ src/backend/access/index/genam.c
@@ -126,6 +126,8 @@ RelationGetIndexScan(Relation indexRelation, int nkeys, int norderbys)
 	scan->xs_hitup = NULL;
 	scan->xs_hitupdesc = NULL;
 
+	scan->xs_visited_pages_limit = 0;
+
 	return scan;
 }
 
@@ -454,7 +456,7 @@ systable_beginscan(Relation heapRelation,
 				elog(ERROR, "column is not in index");
 		}
 
-		sysscan->iscan = index_beginscan(heapRelation, irel,
+		sysscan->iscan = index_beginscan(heapRelation, irel, false,
 										 snapshot, NULL, nkeys, 0,
 										 SO_NONE);
 		index_rescan(sysscan->iscan, idxkey, nkeys, NULL, 0);
@@ -518,7 +520,8 @@ systable_getnext(SysScanDesc sysscan)
 
 	if (sysscan->irel)
 	{
-		if (index_getnext_slot(sysscan->iscan, ForwardScanDirection, sysscan->slot))
+		if (table_index_getnext_slot(sysscan->iscan, ForwardScanDirection,
+									 sysscan->slot))
 		{
 			bool		shouldFree;
 
@@ -643,7 +646,7 @@ systable_endscan(SysScanDesc sysscan)
  * we could do a heapscan and sort, but the uses are in places that
  * probably don't need to still work with corrupted catalog indexes.)
  * For the moment, therefore, these functions are merely the thinest of
- * wrappers around index_beginscan/index_getnext_slot.  The main reason for
+ * wrappers around index_beginscan/table_index_getnext_slot.  The main reason for
  * their existence is to centralize possible future support of lossy operators
  * in catalog scans.
  */
@@ -716,7 +719,7 @@ systable_beginscan_ordered(Relation heapRelation,
 	if (TransactionIdIsValid(CheckXidAlive))
 		bsysscan = true;
 
-	sysscan->iscan = index_beginscan(heapRelation, indexRelation,
+	sysscan->iscan = index_beginscan(heapRelation, indexRelation, false,
 									 snapshot, NULL, nkeys, 0,
 									 SO_NONE);
 	index_rescan(sysscan->iscan, idxkey, nkeys, NULL, 0);
@@ -736,7 +739,7 @@ systable_getnext_ordered(SysScanDesc sysscan, ScanDirection direction)
 	HeapTuple	htup = NULL;
 
 	Assert(sysscan->irel);
-	if (index_getnext_slot(sysscan->iscan, direction, sysscan->slot))
+	if (table_index_getnext_slot(sysscan->iscan, direction, sysscan->slot))
 		htup = ExecFetchSlotHeapTuple(sysscan->slot, false, NULL);
 
 	/* See notes in systable_getnext */
