b444ff65ff9ba4a34b5025c3b2ebe70da69ee1b5
Peter Geoghegan <pg@bowt.ie>
2026-03-26 18:45:27 -0400

    heapam: Keep buffer pins across index scan rescans.
    Avoid dropping the heap page pin (xs_cbuf) and visibility map pin
    (xs_vmbuffer) during heapam_index_fetch_reset.  Retaining these pins
    saves cycles during tight nested loop joins and merge joins that
    frequently restore a saved mark, since the next tuple fetched after a
    rescan often falls on the same heap page.  It can also avoid repeated
    pinning and unpinning of the same buffer when rescans happen to revisit
    the same page.
    
    Preparation for an upcoming patch that will add the amgetbatch
    interface to enable optimizations such as I/O prefetching.
    
    Author: Peter Geoghegan <pg@bowt.ie>
    Reviewed-By: Andres Freund <andres@anarazel.de>
    Discussion: https://postgr.es/m/CAH2-Wz=g=JTSyDB4UtB5su2ZcvsS7VbP+ZMvvaG6ABoCb+s8Lw@mail.gmail.com

--- src/backend/access/index/indexam.c
+++ src/backend/access/index/indexam.c
@@ -386,7 +386,7 @@ index_rescan(IndexScanDesc scan,
 	Assert(nkeys == scan->numberOfKeys);
 	Assert(norderbys == scan->numberOfOrderBys);
 
-	/* Release resources (like buffer pins) from table accesses */
+	/* reset table AM state for rescan */
 	if (scan->xs_heapfetch)
 		table_index_fetch_reset(scan->xs_heapfetch);
 
@@ -462,7 +462,7 @@ index_restrpos(IndexScanDesc scan)
 	SCAN_CHECKS;
 	CHECK_SCAN_PROCEDURE(amrestrpos);
 
-	/* release resources (like buffer pins) from table accesses */
+	/* reset table AM state for restoring the marked position */
 	if (scan->xs_heapfetch)
 		table_index_fetch_reset(scan->xs_heapfetch);
 
@@ -656,7 +656,7 @@ index_getnext_tid(IndexScanDesc scan, ScanDirection direction)
 	/* If we're out of index entries, we're done */
 	if (!found)
 	{
-		/* release resources (like buffer pins) from table accesses */
+		/* reset table AM state */
 		if (scan->xs_heapfetch)
 			table_index_fetch_reset(scan->xs_heapfetch);
 
